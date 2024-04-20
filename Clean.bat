7a7cf6b6d1ec9e93d33e4b2cd471c6865a1bfc4e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-20 15:15:52 +0800

    add a clean script for clean Debug and Release output files.

---
+++ Clean.bat
@@ -0,0 +1,12 @@
+del /S /Q ".vs\ASM-Shelter\v17\ipch\AutoPCH"
+del /S /Q ".vs\ASM-Shelter\v17\Browse.VC.db"
+
+rd /S /Q "Debug"
+rd /S /Q "Release"
+rd /S /Q "x86"
+rd /S /Q "x64"
+
+rd /S /Q "example\shelter\x86"
+rd /S /Q "example\shelter\x64"
+rd /S /Q "example\shelter\Debug"
+rd /S /Q "example\shelter\Release"
