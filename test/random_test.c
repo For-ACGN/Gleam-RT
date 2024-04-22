9abbd3e4e520b9077d97d3e79d578ee57a245179
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-22 17:25:15 +0800

    random_test.c: finish TestRandBuf, TestRandByte and TestRandUint.

--- test/random_test.c
+++ test/random_test.c
@@ -4,24 +4,20 @@
 #include "random.h"
 
 void TestRandBuf();
+void TestRandByte();
+void TestRandUint();
 
 bool TestRandom()
 {
     TestRandBuf();
-
-    
-    
-
-
-
-
-
+    TestRandByte();
+    TestRandUint();
     return true;
 }
 
 void TestRandBuf()
 {
-    printf("========TestRandBuf begin=========\n");
+    printf("========TestRandBuf begin==========\n");
 
     byte buf[16];
     RandBuf(&buf[0], 16);
@@ -33,5 +29,49 @@ void TestRandBuf()
     }
     printf("\n");
 
-    printf("========TestRandBuf passed========\n");
+    printf("========TestRandBuf passed=========\n\n");
+}
+
+void TestRandByte()
+{
+    printf("========TestRandByte begin=========\n");
+
+    // will same
+    for (uint i = 0; i < 3; i++)
+    {
+        printf("byte: %d\n", RandByte(0));
+    }
+    printf("\n");
+
+    // will different
+    uint last = (uint)(&TestRandBuf);
+    for (uint i = 0; i < 3; i++)
+    {
+        last += RandByte(last);
+        printf("byte: %d\n", (byte)last);
+    }
+
+    printf("========TestRandByte passed========\n\n");
+}
+
+void TestRandUint()
+{
+    printf("========TestRandUint begin=========\n");
+
+    // will same
+    for (uint i = 0; i < 3; i++)
+    {
+        printf("uint: %lld\n", RandUint(0));
+    }
+    printf("\n");
+
+    // will different
+    uint last = (uint)(&TestRandBuf);
+    for (uint i = 0; i < 3; i++)
+    {
+        last += RandUint(last);
+        printf("uint: %lld\n", last);
+    }
+
+    printf("========TestRandUint passed========\n\n");
 }
