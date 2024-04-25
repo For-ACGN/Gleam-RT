12039960006a7a99b4269956e76be1b1e8c48808
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-25 16:12:16 +0800

    runtime.c: finish Clean when failed to initialize runtime,update methods.

--- src/runtime.c
+++ src/runtime.c
@@ -3,10 +3,15 @@
 #include "random.h"
 #include "runtime.h"
 
+// hard encoded address in methods for replace
 #ifdef _WIN64
-    #define METHOD_ADDR_HIDE 0x7FFFFFFFFFFFFFF0
+    #define METHOD_ADDR_HIDE    0x7FFFFFFFFFFFFFF0
+    #define METHOD_ADDR_RECOVER 0x7FFFFFFFFFFFFFF1
+    #define METHOD_ADDR_CLEAN   0x7FFFFFFFFFFFFFF2
 #elif _WIN32
-    #define METHOD_ADDR_HIDE 0x7FFFFFF0
+    #define METHOD_ADDR_HIDE    0x7FFFFFF0
+    #define METHOD_ADDR_RECOVER 0x7FFFFFF1
+    #define METHOD_ADDR_CLEAN   0x7FFFFFF2
 #endif
 
 typedef struct {
@@ -44,19 +49,42 @@ Runtime_M* InitRuntime(FindAPI_t findAPI)
         return NULL;
     }
     RandBuf((byte*)address, 4096);
-
+    // initialize runtime
     Runtime* runtime = (Runtime*)(address + 128);
     runtime->FindAPI = findAPI;
-
-
-
-    if (!updateRuntimePointers(runtime))
+    bool success = true;
+    for (;;)
     {
-        // clean 
+
+
+
+
+        if (!updateRuntimePointers(runtime))
+        {
+            success = false;
+            break;
+        }
+        break;
+    }
+    if (!success)
+    {
+        RandBuf((byte*)address, 4096);
+    #ifdef _WIN64
+        uint64 hash = 0x04989F7862AEABA4;
+        uint64 key  = 0xC7825C3DB35EE20E;
+    #elif _WIN32
+        uint32 hash = 0xF76A2ADE;
+        uint32 key  = 0x4D8938BD;
+    #endif
+        VirtualFree virtualFree = (VirtualFree)findAPI(hash, key);
+        if (virtualFree == NULL)
+        {
+            return NULL;
+        }
+        virtualFree(address, 0, MEM_RELEASE);
         return NULL;
     }
-
-
+    // create methods about Runtime
     Runtime_M* module = (Runtime_M*)(address + 2048);
     module->Hide    = &Hide;
     module->Recover = &Recover;
@@ -80,15 +108,14 @@ static bool updateRuntimePointers(Runtime* runtime)
     {
         return false;
     }
-    uintptr begin = (uintptr)(&Hide);
-    uintptr size  = 8192;
-
+    uintptr memBegin = (uintptr)(&Hide);
+    uintptr memSize  = 8192;
+    // change memory protect
     uint32 old;
-    if (!virtualProtect(begin, size, PAGE_EXECUTE_READWRITE, &old))
+    if (!virtualProtect(memBegin, memSize, PAGE_EXECUTE_READWRITE, &old))
     {
         return false;
     }
-
     bool success = true;
     for(;;)
     {
@@ -97,11 +124,20 @@ static bool updateRuntimePointers(Runtime* runtime)
             success =  false;
             break;
         }
+        if (!updateRuntimePointer(runtime, &Recover, METHOD_ADDR_RECOVER))
+        {
+            success = false;
+            break;
+        }
+        if (!updateRuntimePointer(runtime, &Clean, METHOD_ADDR_CLEAN))
+        {
+            success = false;
+            break;
+        }
         break;
     }
-
     // recovery memory protect
-    if (!virtualProtect(begin, size, old, &old))
+    if (!virtualProtect(memBegin, memSize, old, &old))
     {
         return false;
     }
@@ -113,7 +149,7 @@ static bool updateRuntimePointer(Runtime* runtime, void* method, uintptr address
     bool success = false;
     uintptr target = (uintptr)method;
     uintptr* pointer;
-    for (uintptr i = 0; i < 64; i++)
+    for (uintptr i = 0; i < 32; i++)
     {
         pointer = (uintptr*)(target);
         if (*pointer == address)
@@ -132,17 +168,21 @@ __declspec(noinline) void Hide()
     // updatePointer will replace it to the actual address
     Runtime* runtime = (Runtime*)(METHOD_ADDR_HIDE);
 
-
-
-    // return runtime;
+    runtime->FindAPI(0,0);
 }
 
 __declspec(noinline) void Recover()
 {
+    // updatePointer will replace it to the actual address
+    Runtime* runtime = (Runtime*)(METHOD_ADDR_RECOVER);
 
+    runtime->FindAPI(0, 0);
 }
 
 __declspec(noinline) void Clean()
 {
+    // updatePointer will replace it to the actual address
+    Runtime* runtime = (Runtime*)(METHOD_ADDR_CLEAN);
 
+    runtime->FindAPI(0, 0);
 }
