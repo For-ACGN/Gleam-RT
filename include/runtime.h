0464631ecf70ea9a0883a382aba97b3b54123cc3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-25 12:33:29 +0800

    runtime.h: improve typedef about export functions, design dynamic update pointer.

--- include/runtime.h
+++ include/runtime.h
@@ -4,23 +4,23 @@
 #include "go_types.h"
 #include "windows_t.h"
 
-typedef void* (*MemAlloc)(uint size);
-typedef void  (*MemFree)(void* addr);
+typedef void* (*MemAlloc_t)(uint size);
+typedef void  (*MemFree_t)(void* addr);
 
-typedef void (*Hide)();
-typedef void (*Recover)();
-typedef void (*Clean)();
+typedef void (*Hide_t)();
+typedef void (*Recover_t)();
+typedef void (*Clean_t)();
 
 typedef struct {
-    MemAlloc     MemAlloc;
-    MemFree      MemFree;
+    MemAlloc_t   MemAlloc;
+    MemFree_t    MemFree;
     VirtualAlloc VirtualAlloc;
     VirtualFree  VirtualFree;
     CreateThread CreateThread;
 
-    Hide    Hide;
-    Recover Recover;
-    Clean   Clean;
+    Hide_t    Hide;
+    Recover_t Recover;
+    Clean_t   Clean;
 } RuntimeM;
 
 RuntimeM* NewRuntime(FindAPI_t findAPI);
--- src/runtime.c
+++ src/runtime.c
@@ -7,15 +7,16 @@ typedef struct {
     FindAPI_t FindAPI;
 
     // *MemoryMgr MemoryMgr
-
     // *ThreadMgr ThreadMgr
-
-
 } Runtime;
+
+void Hide();
+
+static bool updatePointers(Runtime* runtime, FindAPI_t findAPI);
 
 RuntimeM* NewRuntime(FindAPI_t findAPI)
 {
-    // allocate memory for store MemMgr structure.
+    // allocate memory for store structures.
 #ifdef _WIN64
     uint64 hash = 0xB6A1D0D4A275D4B6;
     uint64 key  = 0x64CB4D66EC0BEFD9;
@@ -35,10 +36,77 @@ RuntimeM* NewRuntime(FindAPI_t findAPI)
     }
     RandBuf((byte*)address, 4096);
 
-    Runtime* runtime = (Runtime*)(address + 128 + RandInt(address) % 512);
-
+    Runtime* runtime = (Runtime*)(address + 128);
     runtime->FindAPI = findAPI;
 
 
-    return 1;
+    if (!updatePointers(runtime, findAPI))
+    {
+        // clean 
+        return NULL;
+    } else {
+        // return 123;
+    }
+
+    RuntimeM* runtimeM = (RuntimeM*)(address+2048);
+    runtimeM->Hide = &Hide;
+    return runtimeM;
+}
+
+static bool updatePointers(Runtime* runtime, FindAPI_t findAPI)
+{
+    // change memory protect for update pointer that hard encode
+#ifdef _WIN64
+    uint64 hash = 0xEA5B0C76C7946815;
+    uint64 key  = 0x8846C203C35DE586;
+#elif _WIN32
+    uint32 hash = 0xB2AC456D;
+    uint32 key  = 0x2A690F63;
+#endif
+    VirtualProtect virtualProtect = (VirtualProtect)findAPI(hash, key);
+    if (virtualProtect == NULL)
+    {
+        return false;
+    }
+    uint32 old;
+    if (!virtualProtect((uintptr)(&Hide), 8192, PAGE_EXECUTE_READWRITE, &old))
+    {
+        return false;
+    }
+    bool ok = false;
+
+    uintptr addr = (uintptr)(&Hide);
+    uintptr* ptr;
+    for (uintptr i = 0; i < 64; i++)
+    {
+        ptr = (uintptr*)(addr);
+        if (*ptr == 0x7FFFFFFFFFFFFFF0)
+        {
+            *ptr = (uintptr)runtime;
+
+            ok = true;
+            break;
+        }
+        addr++;
+    }
+
+
+    // recovery memory protect
+    if (!virtualProtect((uintptr)(&Hide), 8192, old, &old))
+    {
+        return false;
+    }
+    return ok;
+} 
+
+__declspec(noinline) void Hide()
+{
+    // updatePointer will replace to the runtime actual address
+#ifdef _WIN64
+    Runtime* runtime = (Runtime*)(0x7FFFFFFFFFFFFFF0);
+#elif _WIN32
+    Runtime* runtime = (Runtime*)(0x7FFFFFF0);
+#endif
+
+    // return runtime;
 }
