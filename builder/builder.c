1b099edf8b0d5a9172897c31f38ea59d326e9ca4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-25 12:35:00 +0800

    builder.c: update EntryMain about test runtime, add #include runtime.h.

--- builder/builder.c
+++ builder/builder.c
@@ -1,9 +1,15 @@
 ﻿#include "go_types.h"
 #include "hash_api.h"
-#include "memory.h"
+#include "runtime.h"
 
 #pragma comment(linker, "/ENTRY:EntryMain")
 uint EntryMain()
 {
-    return InitMemMgr(&FindAPI);
+    RuntimeM* runtime = NewRuntime(&FindAPI);
+    if (runtime == NULL)
+    {
+        return -1;
+    }
+    runtime->Hide();
+    return 0;
 }
