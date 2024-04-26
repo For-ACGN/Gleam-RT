03d9a28ecb4b77cdcda080782db8adcf301e19a6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-26 16:58:39 +0800

    memory.c: update InitMemoryTracker(), add initTrackerAPI and updateTrackerPointers.

--- src/memory.c
+++ src/memory.c
@@ -6,6 +6,15 @@
 #include "crypto.h"
 #include "memory.h"
 
+// hard encoded address in methods for replace
+#ifdef _WIN64
+    #define METHOD_ADDR_MEMALLOC 0x7FFFFFFFFFFFFF00
+    #define METHOD_ADDR_MEMFREE  0x7FFFFFFFFFFFFF01
+#elif _WIN32
+    #define METHOD_ADDR_MEMALLOC 0x7FFFFF00
+    #define METHOD_ADDR_MEMFREE  0x7FFFFF01
+#endif
+
 typedef struct {
     // API address from runtime
     VirtualAlloc   VirtualAlloc;
@@ -14,21 +23,71 @@ typedef struct {
 
 } MemoryTracker;
 
-// static bool initTrackerAPI(MemoryMgr* manager);
+// methods about memory tracker
+void* MemAlloc(uint size);
+void  MemFree(void* address);
+
+static bool initTrackerAPI(MemoryTracker* tracker, Context* context);
+static bool updateTrackerPointers(MemoryTracker* tracker);
 
 MemoryTracker_M* InitMemoryTracker(Context* context)
 {
-    return NULL;
+    // set structure address
+    uintptr address = context->MemoryPage;
+    uintptr trackerAddr = address + 1000 + RandUint(address) % 256;
+    uintptr moduleAddr  = address + 1300 + RandUint(address) % 256;
+    // initialize tracker
+    MemoryTracker* tracker = (MemoryTracker*)trackerAddr;
+    initTrackerAPI(tracker, context);
+    bool success = true;
+    for (;;)
+    {
+        if (!initTrackerAPI(tracker, context))
+        {
+            success = false;
+            break;
+        }
+        if (!updateTrackerPointers(tracker))
+        {
+            success = false;
+            break;
+        }
+        break;
+    }
+    if (!success)
+    {
+        return NULL;
+    }
+    // create methods about tracker
+    MemoryTracker_M* module = (MemoryTracker_M*)moduleAddr;
+    module->MemAlloc = &MemAlloc;
+    module->MemFree  = &MemFree;
+    return module;
 }
 
+static bool initTrackerAPI(MemoryTracker* tracker, Context* context)
+{
+    tracker->VirtualAlloc   = context->VirtualAlloc;
+    tracker->VirtualFree    = context->VirtualFree;
+    tracker->VirtualProtect = context->VirtualProtect;
+    return true;
+}
 
+static bool updateTrackerPointers(MemoryTracker* tracker)
+{
+    return true;
+}
 
 __declspec(noinline) void* MemAlloc(uint size)
 {
+    // updateTrackerPointers will replace it to the actual address
+    MemoryTracker* tracker = (MemoryTracker*)(METHOD_ADDR_MEMALLOC);
 
 }
 
 __declspec(noinline) void MemFree(void* address)
 {
+    // updateTrackerPointers will replace it to the actual address
+    MemoryTracker* tracker = (MemoryTracker*)(METHOD_ADDR_MEMFREE);
 
 }
