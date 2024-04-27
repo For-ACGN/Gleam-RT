c10d8a5b54363bc2a931c9c8568280f90511844e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-27 16:39:27 +0800

    windows_t.h: add more Windows API for MemoryTracker.

--- include/windows_t.h
+++ include/windows_t.h
@@ -8,11 +8,13 @@
 * https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualalloc
 * https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfree
 * https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
-* https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-flushinstructioncache
 * https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createthread
+* https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-flushinstructioncache
+* https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-createmutexa
+* https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-releasemutex
+* https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
+* https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
 */
-
-typedef uint HANDLE;
 
 #define MEM_COMMIT  0x00001000
 #define MEM_RESERVE 0x00002000
@@ -26,6 +28,9 @@ typedef uint HANDLE;
 #define PAGE_EXECUTE_READ      0x20
 #define PAGE_EXECUTE_READWRITE 0x40
 #define PAGE_EXECUTE_WRITECOPY 0x80
+
+typedef uint  HANDLE;
+typedef byte* LPCSTR;
 
 typedef uintptr (*VirtualAlloc)
 (
@@ -42,15 +47,35 @@ typedef bool (*VirtualProtect)
     uintptr lpAddress, uint dwSize, uint32 flNewProtect, uint32* lpflOldProtect
 );
 
-typedef bool (*FlushInstCache)
-(
-    HANDLE hProcess, uintptr lpBaseAddress, uint dwSize
-);
-
 typedef HANDLE (*CreateThread)
 (
     uintptr lpThreadAttributes, uint dwStackSize, uintptr lpStartAddress,
     uintptr lpParameter, uint32 dwCreationFlags, uint32* lpThreadId
+);
+
+typedef bool (*FlushInstructionCache)
+(
+    HANDLE hProcess, uintptr lpBaseAddress, uint dwSize
+);
+
+typedef HANDLE (*CreateMutexA)
+(
+    uintptr lpMutexAttributes, bool bInitialOwner, LPCSTR lpName
+);
+
+typedef bool (*ReleaseMutex)
+(
+    HANDLE hMutex
+);
+
+typedef uint32 (*WaitForSingleObject)
+(
+    HANDLE hHandle, uint32 dwMilliseconds
+);
+
+typedef bool (*CloseHandle)
+(
+    HANDLE hObject
 );
 
 #endif // WINDOWS_T_H
