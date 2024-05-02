c9dce9010e04194cb1c879e12924b4cda79c7f01
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-02 16:40:49 +0800

    hash_api.h: refactor arguments for FindAPI_t and FindAPI.

--- include/hash_api.h
+++ include/hash_api.h
@@ -3,17 +3,9 @@
 
 #include "go_types.h"
 
-#ifdef _WIN64
-    typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key);
-#elif _WIN32
-    typedef uintptr (*FindAPI_t)(uint32 hash, uint32 key);
-#endif
+typedef uintptr (*FindAPI_t)(uint hash, uint key);
 
 // FindAPI is used to FindAPI address by hash and key.
-#ifdef _WIN64
-    uintptr FindAPI(uint64 hash, uint64 key);
-#elif _WIN32
-    uintptr FindAPI(uint32 hash, uint32 key);
-#endif
+uintptr FindAPI(uint hash, uint key);
 
 #endif // HASH_API_H
