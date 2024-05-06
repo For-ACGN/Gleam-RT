0a9c24ffc9fbf4f200887dab96eef60c4b03ed9c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-06 14:20:12 +0800

    windows_t.h: add suffix _t to Windows API function type.

--- include/windows_t.h
+++ include/windows_t.h
@@ -24,6 +24,14 @@
 * https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
 */
 
+#ifndef _WINDOWS_
+#define _WINDOWS_
+
+typedef uint    HMODULE;
+typedef uint    HANDLE;
+typedef HANDLE* LPHANDLE;
+typedef byte*   LPCSTR;
+
 #define CURRENT_PROCESS (HANDLE)(-1)
 #define CURRENT_THREAD  (HANDLE)(-2)
 
@@ -31,106 +39,103 @@
 #define MEM_RESERVE 0x00002000
 #define MEM_RELEASE 0x00008000
 
-#define PAGE_NOACCESS          0x01
-#define PAGE_READONLY          0x02
-#define PAGE_READWRITE         0x04
-#define PAGE_WRITECOPY         0x08
-#define PAGE_EXECUTE           0x10
-#define PAGE_EXECUTE_READ      0x20
-#define PAGE_EXECUTE_READWRITE 0x40
-#define PAGE_EXECUTE_WRITECOPY 0x80
+#define PAGE_NOACCESS          0x00000001
+#define PAGE_READONLY          0x00000002
+#define PAGE_READWRITE         0x00000004
+#define PAGE_WRITECOPY         0x00000008
+#define PAGE_EXECUTE           0x00000010
+#define PAGE_EXECUTE_READ      0x00000020
+#define PAGE_EXECUTE_READWRITE 0x00000040
+#define PAGE_EXECUTE_WRITECOPY 0x00000080
 
 #define INFINITE      0xFFFFFFFF
 #define WAIT_OBJECT_0 0x00000000
 
 #define DUPLICATE_SAME_ACCESS 0x00000002
 
-typedef uint    HMODULE;
-typedef uint    HANDLE;
-typedef HANDLE* LPHANDLE;
-typedef byte*   LPCSTR;
+#endif // _WINDOWS_
 
-typedef uintptr (*GetProcAddress)
+typedef uintptr (*GetProcAddress_t)
 (
     HMODULE hModule, LPCSTR lpProcName
 );
 
-typedef uintptr (*VirtualAlloc)
+typedef uintptr (*VirtualAlloc_t)
 (
     uintptr lpAddress, uint dwSize, uint32 flAllocationType, uint32 flProtect
 );
 
-typedef bool (*VirtualFree)
+typedef bool (*VirtualFree_t)
 (
     uintptr lpAddress, uint dwSize, uint32 dwFreeType
 );
 
-typedef bool (*VirtualProtect)
+typedef bool (*VirtualProtect_t)
 (
     uintptr lpAddress, uint dwSize, uint32 flNewProtect, uint32* lpflOldProtect
 );
 
-typedef HANDLE (*CreateThread)
+typedef HANDLE (*CreateThread_t)
 (
     uintptr lpThreadAttributes, uint dwStackSize, uintptr lpStartAddress,
     uintptr lpParameter, uint32 dwCreationFlags, uint32* lpThreadId
 );
 
-typedef void (*ExitThread)
+typedef void (*ExitThread_t)
 (
     uint32 dwExitCode
 );
 
-typedef uint32 (*SuspendThread)
+typedef uint32 (*SuspendThread_t)
 (
     HANDLE hThread
 );
 
-typedef uint32 (*ResumeThread)
+typedef uint32 (*ResumeThread_t)
 (
     HANDLE hThread
 );
 
-typedef uint32 (*GetThreadID)
+typedef uint32 (*GetThreadID_t)
 (
     HANDLE hThread
 );
 
-typedef uint32 (*GetCurrentThreadID)();
+typedef uint32 (*GetCurrentThreadID_t)();
 
-typedef bool (*TerminateThread)
+typedef bool (*TerminateThread_t)
 (
     HANDLE hThread, uint32 dwExitCode
 );
 
-typedef bool (*FlushInstructionCache)
+typedef bool (*FlushInstructionCache_t)
 (
     HANDLE hProcess, uintptr lpBaseAddress, uint dwSize
 );
 
-typedef HANDLE (*CreateMutexA)
+typedef HANDLE (*CreateMutexA_t)
 (
     uintptr lpMutexAttributes, bool bInitialOwner, LPCSTR lpName
 );
 
-typedef bool (*ReleaseMutex)
+typedef bool (*ReleaseMutex_t)
 (
     HANDLE hMutex
 );
 
-typedef uint32 (*WaitForSingleObject)
+typedef uint32 (*WaitForSingleObject_t)
 (
     HANDLE hHandle, uint32 dwMilliseconds
 );
 
-typedef bool (*DuplicateHandle)
+typedef bool (*DuplicateHandle_t)
 (
     HANDLE hSourceProcessHandle, HANDLE hSourceHandle,
     HANDLE hTargetProcessHandle, LPHANDLE lpTargetHandle,
     uint32 dwDesiredAccess, bool bInheritHandle, uint32 dwOptions
 );
 
-typedef bool (*CloseHandle)
+typedef bool (*CloseHandle_t)
 (
     HANDLE hObject
 );
