2eac59826eb66c44c47c7f1847cd49fcb1ffff4c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-16 15:15:37 +0800

    testdata/go/build.bat: add set GOARCH=amd64 and 386 for different arch.

--- test/testdata/go/build.bat
+++ test/testdata/go/build.bat
@@ -1,1 +1,4 @@
-go build -v -trimpath -ldflags "-s -w" -o test_go.exe main.go
+set GOARCH=amd64
+go build -v -trimpath -ldflags "-s -w" -o go_amd64.exe main.go
+set GOARCH=386
+go build -v -trimpath -ldflags "-s -w" -o go_386.exe main.go
