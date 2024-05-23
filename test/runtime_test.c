bffde6dc63a5037d6182cfde56737adde08080a8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-23 20:06:59 +0800

    runtime_test.c: replace runtime->Alloc to runtime->MemAlloc, Free to MemFree.

--- test/runtime_test.c
+++ test/runtime_test.c
@@ -29,21 +29,21 @@ static bool TestRuntimeMemory(Runtime_M* runtime)
 {
     printf("======TestRuntimeAlloc begin=======\n");
 
-    uint64* test1 = (uint64*)runtime->Alloc(sizeof(uint64));
+    uint64* test1 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test1 == NULL)
     {
         return -1;
     }
     *test1 = 0x1234567812345600;
 
-    uint64* test2 = (uint64*)runtime->Alloc(sizeof(uint64));
+    uint64* test2 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test2 == NULL)
     {
         return -1;
     }
     *test2 = 0x1234567812345601;
 
-    uint64* test3 = (uint64*)runtime->Alloc(sizeof(uint64));
+    uint64* test3 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test3 == NULL)
     {
         return -1;
@@ -59,22 +59,22 @@ static bool TestRuntimeMemory(Runtime_M* runtime)
         return -9;
     }
 
-    uint64* test4 = (uint64*)runtime->Alloc(sizeof(uint64));
+    uint64* test4 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test3 == NULL)
     {
         return -1;
     }
     *test4 = 0x1234567812345603;
 
-    if (!runtime->Free(test3))
+    if (!runtime->MemFree(test3))
     {
         return -1;
     }
-    if (!runtime->Free(test1))
+    if (!runtime->MemFree(test1))
     {
         return -1;
     }
-    uint64* test5 = (uint64*)runtime->Alloc(sizeof(uint64));
+    uint64* test5 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test5 == NULL)
     {
         return -1;
@@ -96,15 +96,15 @@ static bool TestRuntimeMemory(Runtime_M* runtime)
         return -100;
     }
 
-    if (!runtime->Free(test2))
+    if (!runtime->MemFree(test2))
     {
         return -1;
     }
-    if (!runtime->Free(test4))
+    if (!runtime->MemFree(test4))
     {
         return -1;
     }
-    if (!runtime->Free(test5))
+    if (!runtime->MemFree(test5))
     {
         return -1;
     }
