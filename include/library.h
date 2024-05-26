e5e7d810acd64dba7c47af6e5570bfce6a5c702e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-26 15:06:56 +0800

    library.h: rename LibClean to LibFreeAll, add FreeLibraryAndExitThread.

--- include/library.h
+++ include/library.h
@@ -5,16 +5,18 @@
 #include "windows_t.h"
 #include "context.h"
 
-typedef bool (*LibClean_t)();
+typedef bool (*LibFreeAll_t)();
 
 typedef struct {
     LoadLibraryA_t   LoadLibraryA;
     LoadLibraryW_t   LoadLibraryW;
     LoadLibraryExA_t LoadLibraryExA;
     LoadLibraryExW_t LoadLibraryExW;
-    FreeLibrary_t    FreeLibrary;
 
-    LibClean_t LibClean;
+    FreeLibrary_t              FreeLibrary;
+    FreeLibraryAndExitThread_t FreeLibraryAndExitThread;
+
+    LibFreeAll_t LibFreeAll;
 } LibraryTracker_M;
 
 LibraryTracker_M* InitLibraryTracker(Context* context);
