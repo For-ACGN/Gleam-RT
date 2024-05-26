5ae8feab44b83f7cd4a4b8407f94778708dad875
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-26 14:41:49 +0800

    library.h: add LibraryTracker_M and InitLibraryTracker.

---
+++ include/library.h
@@ -0,0 +1,22 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "c_types.h"
+#include "windows_t.h"
+#include "context.h"
+
+typedef bool (*LibClean_t)();
+
+typedef struct {
+    LoadLibraryA_t   LoadLibraryA;
+    LoadLibraryW_t   LoadLibraryW;
+    LoadLibraryExA_t LoadLibraryExA;
+    LoadLibraryExW_t LoadLibraryExW;
+    FreeLibrary_t    FreeLibrary;
+
+    LibClean_t LibClean;
+} LibraryTracker_M;
+
+LibraryTracker_M* InitLibraryTracker(Context* context);
+
+#endif // LIBRARY_H
