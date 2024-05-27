649d5091f6ea373aa83aaa06befe155051daee4c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-27 14:17:11 +0800

    library.c: add LibraryTracker and InitLibraryTracker.

---
+++ src/library.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#include "c_types.h"
+#include "windows_t.h"
+#include "hash_api.h"
+#include "list_md.h"
+#include "context.h"
+#include "random.h"
+#include "crypto.h"
+#include "library.h"
+
+typedef struct {
+    HMODULE hModule;
+} module;
+
+typedef struct {
+    // API addresses
+    LoadLibraryA_t             LoadLibraryA;
+    LoadLibraryW_t             LoadLibraryW;
+    LoadLibraryExA_t           LoadLibraryExA;
+    LoadLibraryExW_t           LoadLibraryExW;
+    FreeLibrary_t              FreeLibrary;
+    FreeLibraryAndExitThread_t FreeLibraryAndExitThread;
+
+    // runtime data
+    HANDLE Mutex; // global mutex
+
+    // store all modules info
+    List Modules;
+    byte ModulesKey[CRYPTO_KEY_SIZE];
+    byte ModulesIV [CRYPTO_IV_SIZE];
+} LibraryTracker;
+
+LibraryTracker_M* InitLibraryTracker(Context* context)
+{
+    // set structure address
+    uintptr address = context->MainMemPage;
+    uintptr trackerAddr = address + 3500 + RandUint(address) % 256;
+    uintptr moduleAddr  = address + 4100 + RandUint(address) % 256;
+    // initialize tracker
+    LibraryTracker* tracker = (LibraryTracker*)trackerAddr;
+    uint errCode = 0;
+    return NULL;
+}
