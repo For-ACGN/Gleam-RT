913b0b9fda8c6217b9992e9873835db8395b9efb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-01 20:09:34 +0800

    win_api.h: rename file winapi.h to win_api.h, winapi.c to win_api.c.

--- include/win_api.h
+++ include/win_api.h
@@ -1,9 +1,9 @@
-#ifndef WINAPI_H
-#define WINAPI_H
+#ifndef WIN_API_H
+#define WIN_API_H
 
 #include "c_types.h"
 #include "windows_t.h"
 
 uint32 GetModuleFileName(HMODULE hModule, byte* name, uint32 size);
 
-#endif // WINAPI_H
+#endif // WIN_API_H
