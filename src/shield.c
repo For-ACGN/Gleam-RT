cbf372854a7439e8dbe86c54de212f6421673995
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-02 15:48:05 +0800

    shield.c: improve code about hide and recover runtime instructions.

--- src/shield.c
+++ src/shield.c
@@ -1,10 +1,10 @@
-#include "stdio.h"
-
 #include "c_types.h"
 #include "random.h"
 #include "shield.h"
 
 #define XOR_KEY_SIZE 256
+
+void xorInst(Shield_Ctx* ctx, byte* key);
 
 __declspec(noinline)
 bool DefenseRT(Shield_Ctx* ctx)
@@ -12,23 +12,27 @@ bool DefenseRT(Shield_Ctx* ctx)
     // generate random key
     byte key[XOR_KEY_SIZE];
     RandBuf(&key[0], XOR_KEY_SIZE);
+    // hide runtime(or with shellcode) instructions
+    xorInst(ctx, &key[0]);
+    // simulate kernel32.Sleep()
+    bool success = ctx->WaitForSingleObject(ctx->hProcess, ctx->milliseconds);
+    // recover runtime(or with shellcode) instructions
+    xorInst(ctx, &key[0]);
+    return success;
+}
+
+void xorInst(Shield_Ctx* ctx, byte* key)
+{
     // calculate shellcode position
     uintptr beginAddr = ctx->InstAddress;
     uintptr endAddr   = (uintptr)(&DefenseRT);
-
-    printf("begin: %llX\n", beginAddr);
-    printf("end: %llX\n", endAddr);
-
     // hide runtime(or with shellcode) instructions
-    byte last   = 170;
     uint keyIdx = 0;
     for (uintptr addr = beginAddr; addr < endAddr; addr++)
     {
         byte* data = (byte*)addr;
-        byte  enc  = *data ^ key[keyIdx] ^ last;
-        // update status
-        *data = enc;
-        last  = enc;
+        byte k = *(byte*)(key + keyIdx);
+        *data ^= k;
         // update key index
         keyIdx++;
         if (keyIdx >= XOR_KEY_SIZE)
@@ -36,24 +40,4 @@ bool DefenseRT(Shield_Ctx* ctx)
             keyIdx = 0;
         }
     }
-    // simulate kernel32.Sleep()
-    bool success = ctx->WaitForSingleObject(ctx->hProcess, ctx->milliseconds);
-    // recover runtime(or with shellcode) instructions
-    last   = 170;
-    keyIdx = 0;
-    for (uintptr addr = beginAddr; addr < endAddr; addr++)
-    {
-        byte* data = (byte*)addr;
-        byte  enc = *data ^ key[keyIdx] ^ last;
-        // update status
-        *data = enc;
-        last = enc;
-        // update key index
-        keyIdx++;
-        if (keyIdx >= XOR_KEY_SIZE)
-        {
-            keyIdx = 0;
-        }
-    }
-    return success;
 }
