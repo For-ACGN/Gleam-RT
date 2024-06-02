b57b084a506f8da9980a72f4e9119e36c4b49c86
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-02 14:19:16 +0800

    shield.h: add Shield_Ctx structure and DefenseRT function.

---
+++ include/shield.h
@@ -0,0 +1,17 @@
+#ifndef SHIELD_H
+#define SHIELD_H
+
+#include "c_types.h"
+#include "windows_t.h"
+
+typedef struct {
+    uintptr InstAddress;
+    uint32  milliseconds;
+	HANDLE  hProcess;
+	
+	WaitForSingleObject_t WaitForSingleObject;
+} Shield_Ctx;
+
+bool DefenseRT(Shield_Ctx* ctx);
+
+#endif // SHIELD_H
