6357f61912a433c5188ee53dc702ac2eaed052f3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-04 16:25:22 +0800

    resource.h: add ResourceTracker_M and InitResourceTracker.

---
+++ include/resource.h
@@ -0,0 +1,23 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include "c_types.h"
+#include "windows_t.h"
+#include "context.h"
+#include "errno.h"
+
+typedef errno (*ResEncrypt_t)();
+typedef errno (*ResDecrypt_t)();
+typedef errno (*ResClean_t)();
+
+typedef struct {
+
+
+    ResEncrypt_t ResEncrypt;
+    ResDecrypt_t ResDecrypt;
+    ResClean_t   ResClean;
+} ResourceTracker_M;
+
+ResourceTracker_M* InitResourceTracker(Context* context);
+
+#endif // RESOURCE_H
