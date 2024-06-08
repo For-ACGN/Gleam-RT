5c473938e3d8e00e5e28f5ef42971a94597df144
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-08 13:46:57 +0800

    win_api.h: add PE_Info structure and ParsePEImage function.

--- include/win_api.h
+++ include/win_api.h
@@ -4,6 +4,13 @@
 #include "c_types.h"
 #include "windows_t.h"
 
+typedef struct {
+    uintptr EntryPoint;
+    uintptr ImageBase;
+    uint32  ImageSize;
+} PE_Info;
+
 uint32 GetModuleFileName(HMODULE hModule, byte* name, uint32 size);
+void   ParsePEImage(void* address, PE_Info* info);
 
 #endif // WIN_API_H
