0b9c5445cdfff004909594a30f400f9ab499e5dc
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-09 14:29:34 +0800

    win_api.c: update ParsePEImage about parse sections and search .text.

--- src/win_api.c
+++ src/win_api.c
@@ -53,7 +53,10 @@ void ParsePEImage(void* address, PE_Info* info)
 {
     uintptr imageAddr = (uintptr)address;
     uint32  peOffset  = *(uint32*)(imageAddr + 60);
-
+    // parse file header
+    uint16 numSections   = *(uint16*)(imageAddr + peOffset + 6);
+    uint16 optHeaderSize = *(uint16*)(imageAddr + peOffset + 20);
+    // parse optional header
     uint32  entryPoint = *(uint32*)(imageAddr + peOffset + 40);
 #ifdef _WIN64
     uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 48);
@@ -61,7 +64,23 @@ void ParsePEImage(void* address, PE_Info* info)
     uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 52);
 #endif
     uint32 imageSize = *(uint32*)(imageAddr + peOffset + 80);
-
+    // parse sections and search .text
+    uintptr section = imageAddr + PE_FILE_HEADER_SIZE + peOffset + optHeaderSize;
+    for (uint16 i = 0; i < numSections; i++)
+    {
+        // not record the original ".text" bytes
+        uint64 name = *(uint64*)section ^ 0x000000FFFFFFFFFF;
+        if (name != (0x000000747865742E ^ 0x000000FFFFFFFFFF))
+        {
+            section += PE_SECTION_HEADER_SIZE;
+            continue;
+        }
+        info->TextVirtualSize      = *(uint32*)(section + 8); 
+        info->TextVirtualAddress   = *(uint32*)(section + 12); 
+        info->TextSizeOfRawData    = *(uint32*)(section + 16); 
+        info->TextPointerToRawData = *(uint32*)(section + 20);
+        break;
+    }
     info->EntryPoint = imageAddr + entryPoint;
     info->ImageBase  = imageBase;
     info->ImageSize  = imageSize;
