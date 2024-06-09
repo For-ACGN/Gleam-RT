268ee8078af2c0075aa52ac795ed92dea6bd0b76
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-09 14:28:03 +0800

    win_api.h: add more information about section to PE_Info.

--- include/win_api.h
+++ include/win_api.h
@@ -4,10 +4,20 @@
 #include "c_types.h"
 #include "windows_t.h"
 
+#define PE_FILE_HEADER_SIZE    24
+#define PE_SECTION_HEADER_SIZE 40
+
 typedef struct {
+    // optional header
     uintptr EntryPoint;
     uintptr ImageBase;
     uint32  ImageSize;
+
+    // section information
+    uint32 TextVirtualSize;
+    uint32 TextVirtualAddress;
+    uint32 TextSizeOfRawData;
+    uint32 TextPointerToRawData;
 } PE_Info;
 
 uint32 GetModuleFileName(HMODULE hModule, byte* name, uint32 size);
