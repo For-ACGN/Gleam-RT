ad68d2693c8e7a03e3ed87bb18f359121a483721
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-15 19:01:33 +0800

    hash_api.bat: replace executable file name to hash_api, improve echo title.

--- script/hash_api.bat
+++ script/hash_api.bat
@@ -1,120 +1,120 @@
 @echo off
 
 echo ============================================================
-echo Build HashAPI Tool from https://github.com/For-ACGN/hash_api
+echo Build HashAPI tool from https://github.com/For-ACGN/hash_api
 echo ============================================================
 echo.
 
-echo Runtime Core
-hash -fmt 64 -conc -func GetSystemInfo
-hash -fmt 64 -conc -func FlushInstructionCache
-hash -fmt 64 -conc -func CreateMutexA
-hash -fmt 64 -conc -func ReleaseMutex
-hash -fmt 64 -conc -func CreateEventA
-hash -fmt 64 -conc -func SetEvent
-hash -fmt 64 -conc -func ResetEvent
-hash -fmt 64 -conc -func WaitForSingleObject
-hash -fmt 64 -conc -func DuplicateHandle
-hash -fmt 64 -conc -func CloseHandle
+echo [Runtime Core]
+hash_api -fmt 64 -conc -func GetSystemInfo
+hash_api -fmt 64 -conc -func FlushInstructionCache
+hash_api -fmt 64 -conc -func CreateMutexA
+hash_api -fmt 64 -conc -func ReleaseMutex
+hash_api -fmt 64 -conc -func CreateEventA
+hash_api -fmt 64 -conc -func SetEvent
+hash_api -fmt 64 -conc -func ResetEvent
+hash_api -fmt 64 -conc -func WaitForSingleObject
+hash_api -fmt 64 -conc -func DuplicateHandle
+hash_api -fmt 64 -conc -func CloseHandle
 echo.
 
-echo Runtime IAT Hooks
-hash -fmt 64 -conc -func GetProcAddress
-hash -fmt 64 -conc -func RT_GetProcAddressByName
-hash -fmt 64 -conc -func RT_GetProcAddressByHash
-hash -fmt 64 -conc -func RT_GetProcAddressOriginal
-hash -fmt 64 -conc -func Sleep
+echo [Runtime IAT Hooks]
+hash_api -fmt 64 -conc -func GetProcAddress
+hash_api -fmt 64 -conc -func RT_GetProcAddressByName
+hash_api -fmt 64 -conc -func RT_GetProcAddressByHash
+hash_api -fmt 64 -conc -func RT_GetProcAddressOriginal
+hash_api -fmt 64 -conc -func Sleep
 echo.
 
-echo Library Tracker
-hash -fmt 64 -conc -func LoadLibraryA
-hash -fmt 64 -conc -func LoadLibraryW
-hash -fmt 64 -conc -func LoadLibraryExA
-hash -fmt 64 -conc -func LoadLibraryExW
-hash -fmt 64 -conc -func FreeLibrary
-hash -fmt 64 -conc -func FreeLibraryAndExitThread
+echo [Library Tracker]
+hash_api -fmt 64 -conc -func LoadLibraryA
+hash_api -fmt 64 -conc -func LoadLibraryW
+hash_api -fmt 64 -conc -func LoadLibraryExA
+hash_api -fmt 64 -conc -func LoadLibraryExW
+hash_api -fmt 64 -conc -func FreeLibrary
+hash_api -fmt 64 -conc -func FreeLibraryAndExitThread
 echo.
 
-echo Memory Tracker
-hash -fmt 64 -conc -func VirtualAlloc
-hash -fmt 64 -conc -func VirtualFree
-hash -fmt 64 -conc -func VirtualProtect
-hash -fmt 64 -conc -func VirtualQuery
+echo [Memory Tracker]
+hash_api -fmt 64 -conc -func VirtualAlloc
+hash_api -fmt 64 -conc -func VirtualFree
+hash_api -fmt 64 -conc -func VirtualProtect
+hash_api -fmt 64 -conc -func VirtualQuery
 echo.
 
-echo Thread Tracker
-hash -fmt 64 -conc -func CreateThread
-hash -fmt 64 -conc -func ExitThread
-hash -fmt 64 -conc -func SuspendThread
-hash -fmt 64 -conc -func ResumeThread
-hash -fmt 64 -conc -func GetThreadContext
-hash -fmt 64 -conc -func SetThreadContext
-hash -fmt 64 -conc -func SwitchToThread
-hash -fmt 64 -conc -func GetThreadId
-hash -fmt 64 -conc -func GetCurrentThreadId
-hash -fmt 64 -conc -func TerminateThread
+echo [Thread Tracker]
+hash_api -fmt 64 -conc -func CreateThread
+hash_api -fmt 64 -conc -func ExitThread
+hash_api -fmt 64 -conc -func SuspendThread
+hash_api -fmt 64 -conc -func ResumeThread
+hash_api -fmt 64 -conc -func GetThreadContext
+hash_api -fmt 64 -conc -func SetThreadContext
+hash_api -fmt 64 -conc -func SwitchToThread
+hash_api -fmt 64 -conc -func GetThreadId
+hash_api -fmt 64 -conc -func GetCurrentThreadId
+hash_api -fmt 64 -conc -func TerminateThread
 echo.
 
-echo Resource Tracker
-hash -fmt 64 -conc -mod ws2_32.dll -func WSAStartup
-hash -fmt 64 -conc -mod ws2_32.dll -func WSACleanup
+echo [Resource Tracker]
+hash_api -fmt 64 -conc -mod ws2_32.dll -func WSAStartup
+hash_api -fmt 64 -conc -mod ws2_32.dll -func WSACleanup
 echo.
 
 echo.
 
-echo Runtime Core
-hash -fmt 32 -conc -func GetSystemInfo
-hash -fmt 32 -conc -func FlushInstructionCache
-hash -fmt 32 -conc -func CreateMutexA
-hash -fmt 32 -conc -func ReleaseMutex
-hash -fmt 32 -conc -func CreateEventA
-hash -fmt 32 -conc -func SetEvent
-hash -fmt 32 -conc -func ResetEvent
-hash -fmt 32 -conc -func WaitForSingleObject
-hash -fmt 32 -conc -func DuplicateHandle
-hash -fmt 32 -conc -func CloseHandle
+echo [Runtime Core]
+hash_api -fmt 32 -conc -func GetSystemInfo
+hash_api -fmt 32 -conc -func FlushInstructionCache
+hash_api -fmt 32 -conc -func CreateMutexA
+hash_api -fmt 32 -conc -func ReleaseMutex
+hash_api -fmt 32 -conc -func CreateEventA
+hash_api -fmt 32 -conc -func SetEvent
+hash_api -fmt 32 -conc -func ResetEvent
+hash_api -fmt 32 -conc -func WaitForSingleObject
+hash_api -fmt 32 -conc -func DuplicateHandle
+hash_api -fmt 32 -conc -func CloseHandle
 echo.
 
-echo Runtime IAT Hooks
-hash -fmt 32 -conc -func GetProcAddress
-hash -fmt 32 -conc -func RT_GetProcAddressByName
-hash -fmt 32 -conc -func RT_GetProcAddressByHash
-hash -fmt 32 -conc -func RT_GetProcAddressOriginal
-hash -fmt 32 -conc -func Sleep
+echo [Runtime IAT Hooks]
+hash_api -fmt 32 -conc -func GetProcAddress
+hash_api -fmt 32 -conc -func RT_GetProcAddressByName
+hash_api -fmt 32 -conc -func RT_GetProcAddressByHash
+hash_api -fmt 32 -conc -func RT_GetProcAddressOriginal
+hash_api -fmt 32 -conc -func Sleep
 echo.
 
-echo Library Tracker
-hash -fmt 32 -conc -func LoadLibraryA
-hash -fmt 32 -conc -func LoadLibraryW
-hash -fmt 32 -conc -func LoadLibraryExA
-hash -fmt 32 -conc -func LoadLibraryExW
-hash -fmt 32 -conc -func FreeLibrary
-hash -fmt 32 -conc -func FreeLibraryAndExitThread
+echo [Library Tracker]
+hash_api -fmt 32 -conc -func LoadLibraryA
+hash_api -fmt 32 -conc -func LoadLibraryW
+hash_api -fmt 32 -conc -func LoadLibraryExA
+hash_api -fmt 32 -conc -func LoadLibraryExW
+hash_api -fmt 32 -conc -func FreeLibrary
+hash_api -fmt 32 -conc -func FreeLibraryAndExitThread
 echo.
 
-echo Memory Tracker
-hash -fmt 32 -conc -func VirtualAlloc
-hash -fmt 32 -conc -func VirtualFree
-hash -fmt 32 -conc -func VirtualProtect
-hash -fmt 32 -conc -func VirtualQuery
+echo [Memory Tracker]
+hash_api -fmt 32 -conc -func VirtualAlloc
+hash_api -fmt 32 -conc -func VirtualFree
+hash_api -fmt 32 -conc -func VirtualProtect
+hash_api -fmt 32 -conc -func VirtualQuery
 echo.
 
-echo Thread Tracker
-hash -fmt 32 -conc -func CreateThread
-hash -fmt 32 -conc -func ExitThread
-hash -fmt 32 -conc -func SuspendThread
-hash -fmt 32 -conc -func ResumeThread
-hash -fmt 32 -conc -func GetThreadContext
-hash -fmt 32 -conc -func SetThreadContext
-hash -fmt 32 -conc -func SwitchToThread
-hash -fmt 32 -conc -func GetThreadId
-hash -fmt 32 -conc -func GetCurrentThreadId
-hash -fmt 32 -conc -func TerminateThread
+echo [Thread Tracker]
+hash_api -fmt 32 -conc -func CreateThread
+hash_api -fmt 32 -conc -func ExitThread
+hash_api -fmt 32 -conc -func SuspendThread
+hash_api -fmt 32 -conc -func ResumeThread
+hash_api -fmt 32 -conc -func GetThreadContext
+hash_api -fmt 32 -conc -func SetThreadContext
+hash_api -fmt 32 -conc -func SwitchToThread
+hash_api -fmt 32 -conc -func GetThreadId
+hash_api -fmt 32 -conc -func GetCurrentThreadId
+hash_api -fmt 32 -conc -func TerminateThread
 echo.
 
-echo Resource Tracker
-hash -fmt 32 -conc -mod ws2_32.dll -func WSAStartup
-hash -fmt 32 -conc -mod ws2_32.dll -func WSACleanup
+echo [Resource Tracker]
+hash_api -fmt 32 -conc -mod ws2_32.dll -func WSAStartup
+hash_api -fmt 32 -conc -mod ws2_32.dll -func WSACleanup
 echo.
 
 pause
