d489de80193f00dccf7b5866068579dd24ad2443
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-19 14:27:47 +0800

    shield.h: recover fields about hProcess and WaitForSingleObject.

--- include/shield.h
+++ include/shield.h
@@ -4,12 +4,12 @@
 #include "c_types.h"
 #include "windows_t.h"
 
-typedef bool (*sleep_t)(uint32 milliseconds);
-
 typedef struct {
     uintptr InstAddress;
     uint32  SleepTime;
-	sleep_t Sleep;
+    HANDLE  hProcess;
+
+    WaitForSingleObject_t WaitForSingleObject;
 } Shield_Ctx;
 
 bool DefenseRT(Shield_Ctx* ctx);
