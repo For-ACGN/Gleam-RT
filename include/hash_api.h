bd64913f6a50c194fb7b7f657bf3a1e80610e918
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-22 16:06:32 +0800

    hash_api.h: improve return value type about FindAPI, update HashAPI_W.

--- include/hash_api.h
+++ include/hash_api.h
@@ -3,10 +3,10 @@
 
 #include "c_types.h"
 
-typedef uintptr (*FindAPI_t)(uint hash, uint key);
+typedef void* (*FindAPI_t)(uint hash, uint key);
 
 // FindAPI is used to FindAPI address by hash and key.
-uintptr FindAPI(uint hash, uint key);
+void* FindAPI(uint hash, uint key);
 
 // HashAPI_A is used to calculate Windows API hash by module
 // and function with key, module and function are ASCII.
@@ -16,8 +16,8 @@ uint32 HashAPI32_A(byte* module, byte* function, uint32 key);
 
 // HashAPI_W is used to calculate Windows API hash by module
 // and function with key, module is Unicode, function is ASCII.
-uint   HashAPI_W  (byte* module, byte* function, uint key);
-uint64 HashAPI64_W(byte* module, byte* function, uint64 key);
-uint32 HashAPI32_W(byte* module, byte* function, uint32 key);
+uint   HashAPI_W  (uint16* module, byte* function, uint key);
+uint64 HashAPI64_W(uint16* module, byte* function, uint64 key);
+uint32 HashAPI32_W(uint16* module, byte* function, uint32 key);
 
 #endif // HASH_API_H
