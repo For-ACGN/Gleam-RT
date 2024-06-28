f4aa488218f5389dba33455cd03bf11ecbca897b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-28 17:23:03 +0800

    resource.h: add CloseHandle, FindFirstFileA, FindFirstFileW, FindClose.

--- include/resource.h
+++ include/resource.h
@@ -11,10 +11,15 @@ typedef errno (*ResDecrypt_t)();
 typedef errno (*ResClean_t)();
 
 typedef struct {
-    CreateFileA_t CreateFileA;
-    CreateFileW_t CreateFileW;
-    WSAStartup_t  WSAStartup;
-    WSACleanup_t  WSACleanup;
+    CreateFileA_t    CreateFileA;
+    CreateFileW_t    CreateFileW;
+    CloseHandle_t    CloseHandle;
+    FindFirstFileA_t FindFirstFileA;
+    FindFirstFileW_t FindFirstFileW;
+    FindClose_t      FindClose;
+
+    WSAStartup_t WSAStartup;
+    WSACleanup_t WSACleanup;
 
     ResEncrypt_t ResEncrypt;
     ResDecrypt_t ResDecrypt;
