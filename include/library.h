842814d0b36b768420248e0f092d675405c1cea2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-06-30 14:04:14 +0800

    library.h: add LibLock_t and LibUnlock_t, add LibLock and LibUnlock to LibraryTracker_M.

--- include/library.h
+++ include/library.h
@@ -6,6 +6,8 @@
 #include "context.h"
 #include "errno.h"
 
+typedef bool  (*LibLock_t)();
+typedef bool  (*LibUnlock_t)();
 typedef errno (*LibEncrypt_t)();
 typedef errno (*LibDecrypt_t)();
 typedef errno (*LibClean_t)();
@@ -18,6 +20,8 @@ typedef struct {
     FreeLibrary_t              FreeLibrary;
     FreeLibraryAndExitThread_t FreeLibraryAndExitThread;
 
+    LibLock_t    LibLock;
+    LibUnlock_t  LibUnlock;
     LibEncrypt_t LibEncrypt;
     LibDecrypt_t LibDecrypt;
     LibClean_t   LibClean;
