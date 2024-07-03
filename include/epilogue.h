a317e4ec9299782f990aa3417a05162cdb08761a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-03 17:22:34 +0800

    epilogue.h: add #pragma warning(disable: 4276) for ignore compile error for x86.

--- include/epilogue.h
+++ include/epilogue.h
@@ -1,7 +1,10 @@
 #ifndef EPILOGUE_H
 #define EPILOGUE_H
 
+#pragma warning(push)
+#pragma warning(disable: 4276)
 // Epilogue is used to calculate the total size of runtime instruction.
 void Epilogue();
+#pragma warning(pop)
 
 #endif // EPILOGUE_H
