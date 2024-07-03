b8a4f23bcbcca61c4526c8da93871d1ad7199ddc
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-03 18:07:44 +0800

    memory.c: improve MT_VirtualAlloc for call VirtualFree if failed.

--- src/memory.c
+++ src/memory.c
@@ -304,10 +304,18 @@ LPVOID MT_VirtualAlloc(LPVOID address, SIZE_T size, DWORD type, DWORD protect)
 
     if (!MT_Unlock())
     {
+        if (success)
+        {
+            tracker->VirtualFree(address, 0, MEM_RELEASE);
+        }
         return NULL;
     }
     if (!success)
     {
+        if (address != NULL)
+        {
+            tracker->VirtualFree(address, 0, MEM_RELEASE);
+        }
         return NULL;
     }
     return page;
