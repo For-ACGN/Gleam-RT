40e27c938ae8595c17c0ba3d3ec822b95fefba02
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-06 14:05:24 +0800

    epilogue.c: update Epilogue for prevent it be linked to other empty funtions.

--- src/epilogue.c
+++ src/epilogue.c
@@ -1,6 +1,10 @@
 #include "epilogue.h"
 
+// prevent it be linked to other empty funtions.
+#pragma optimize("", off)
 void Epilogue()
 {
+    char var = 0xFA;
     return;
 }
+#pragma optimize("", on)
