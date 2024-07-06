6b97e32696f4209c7dcd4e2f203e9b3111b0d6a5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-06 14:07:42 +0800

    debug.c: add two dbg_log that is empty when use RELEASE mode.

---
+++ src/debug.c
@@ -0,0 +1,23 @@
+#ifdef IS_RELEASE
+
+void dbg_log(char* mod, char* fmt, ...){};
+
+#else
+
+#include <stdio.h>
+#include <varargs.h>
+#include "c_types.h"
+#include "debug.h"
+
+void dbg_log(char* mod, char* fmt, ...)
+{
+    va_list args;
+    va_start(args);
+
+    printf_s("%s ", mod);
+    printf_s(fmt, args);
+
+    va_end(args);
+}
+
+#endif
