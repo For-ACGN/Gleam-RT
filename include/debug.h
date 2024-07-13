4445a1f5354c65b962e05e158662712e23964785
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-13 14:40:56 +0800

    debug.h: add #ifndef RELEASE_MODE for #define dbg_log(mod, fmt, ...).

--- include/debug.h
+++ include/debug.h
@@ -1,10 +1,21 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include "build.h"
 #include "c_types.h"
+
+#ifndef RELEASE_MODE
 
 bool InitDebugModule();
 
 void dbg_log(char* mod, char* fmt, ...);
+
+#else
+
+bool InitDebugModule();
+
+#define dbg_log(mod, fmt, ...)
+
+#endif
 
 #endif // DEBUG_H
