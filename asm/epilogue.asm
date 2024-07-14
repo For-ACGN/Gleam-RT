b43c0a6589926a2759106001fee396164b7c4056
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-14 14:40:50 +0800

    epilogue.asm: improve define proc for x64 and x86 about Epilogue.

--- asm/epilogue.asm
+++ asm/epilogue.asm
@@ -6,11 +6,15 @@ ENDIF
 
 IFDEF _WIN32
   _Epilogue@0 proc
-    ret
-  _Epilogue@0 endp
 ELSE
   Epilogue proc
-    ret
+ENDIF
+
+  ret
+
+IFDEF _WIN32
+  _Epilogue@0 endp
+ELSE
   Epilogue endp
 ENDIF
 
