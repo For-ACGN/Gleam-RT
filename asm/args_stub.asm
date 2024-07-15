a13ef898e4166ccf60a0972725ac94b0764c8207
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-15 13:39:02 +0800

    args_stub.asm: add total cipher data size, update structure and comment.

--- asm/args_stub.asm
+++ asm/args_stub.asm
@@ -2,15 +2,17 @@ IFDEF _WIN32
 .model tiny
 ENDIF
 
-; +----------+----------+----------+----------+
-; |    key   | num args | arg size | arg data |
-; +----------+----------+----------+----------+
-; | 32 bytes |  uint32  |  uint32  |    var   |
-; +----------+----------+----------+----------+
+; total size(offset) is (32 + 8 * 4) = 64 bytes
+
+; +----------+----------+----------+----------+----------+
+; |    key   |   size   | num args | arg size | arg data |
+; +----------+----------+----------+----------+----------+
+; | 32 bytes |  uint32  |  uint32  |  uint32  |    var   |
+; +----------+----------+----------+----------+----------+
 
 .code
 
-args_stub proc
+Args_Stub proc
   db 11h, 11h, 11h, 11h  ; 32 bytes decrypt key
   db 11h, 11h, 11h, 11h
   db 11h, 11h, 11h, 11h
@@ -20,14 +22,16 @@ args_stub proc
   db 11h, 11h, 11h, 11h
   db 11h, 11h, 11h, 11h
 
+  db 18h, 00h, 00h, 00h  ; total cipher data size
+
   db 02h, 00h, 00h, 00h  ; record the number of the arguments
 
   db 04h, 00h, 00h, 00h  ; record the size of the argument-1
   db 78h, 56h, 34h, 12h  ; argument-1 data
 
   db 09h, 00h, 00h, 00h  ; record the size of the argument-2
-  db "abcdefgh", 00h     ; argument-2 data
+  db "aaaabbbbccc", 00h  ; argument-2 data
 
-args_stub endp
+Args_Stub endp
 
 end
