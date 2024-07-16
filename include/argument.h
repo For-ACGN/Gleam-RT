7e25200b26d70f3b055bf5b5f242a8edd20a36c8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-16 15:03:10 +0800

    argument.h: define macros about the offsets of the arguments structure.

--- include/argument.h
+++ include/argument.h
@@ -1,6 +1,17 @@
 #ifndef ARGUMENT_H
 #define ARGUMENT_H
 
+// +----------+----------+----------+----------+----------+
+// |    key   |   size   | num args | arg size | arg data |
+// +----------+----------+----------+----------+----------+
+// | 32 bytes |  uint32  |  uint32  |  uint32  |    var   |
+// +----------+----------+----------+----------+----------+
+
+#define ARG_OFFSET_CRYPTO_KEY (0+0)
+#define ARG_OFFSET_TOTAL_SIZE (0+32)
+#define ARG_OFFSET_NUM_ARGS   (32+4)
+#define ARG_OFFSET_FIRST_ARG  (36+4)
+
 // reserve stub for store arguments
 extern void Args_Stub();
 
