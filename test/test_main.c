8eed4f3b203e70cfe98349706a04a0bd1328f4e9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-16 13:06:22 +0800

    test_main.c: define int __cdecl main() for process warning on x86 platform.

--- test/test_main.c
+++ test/test_main.c
@@ -10,7 +10,7 @@
 static printBannerBegin(byte* name, uint len);
 static printBannerEnd  (byte* name, uint len, bool pass);
 
-int main()
+int __cdecl main()
 {
     // calculate the banner length
     uint maxNameLen = 0;
