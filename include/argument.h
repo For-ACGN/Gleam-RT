f0c82d60fa0fd08968da01766cc8863cf53475ee
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-17 13:03:26 +0800

    argument.h: refactor argument module, add InitArgumentStore.

--- include/argument.h
+++ include/argument.h
@@ -1,18 +1,29 @@
 #ifndef ARGUMENT_H
 #define ARGUMENT_H
 
-// +----------+----------+----------+----------+----------+
-// |    key   |   size   | num args | arg size | arg data |
-// +----------+----------+----------+----------+----------+
-// | 32 bytes |  uint32  |  uint32  |  uint32  |    var   |
-// +----------+----------+----------+----------+----------+
+#include "c_types.h"
+#include "context.h"
+#include "errno.h"
 
-#define ARG_OFFSET_CRYPTO_KEY (0+0)
-#define ARG_OFFSET_TOTAL_SIZE (0+32)
-#define ARG_OFFSET_NUM_ARGS   (32+4)
-#define ARG_OFFSET_FIRST_ARG  (36+4)
+typedef void* (*ArgGet_t)(uint index);
+typedef errno (*ArgEncrypt_t)();
+typedef errno (*ArgDecrypt_t)();
+typedef errno (*ArgClean_t)();
 
+typedef struct {
+    ArgGet_t Get;
+
+    ArgEncrypt_t Encrypt;
+    ArgDecrypt_t Decrypt;
+    ArgClean_t   Clean;
+} ArgumentStore_M;
+
+ArgumentStore_M* InitArgumentStore(Context* context);
+
+#pragma warning(push)
+#pragma warning(disable: 4276)
 // reserve stub for store arguments
 extern void Args_Stub();
+#pragma warning(pop)
 
 #endif // ARGUMENT_H
