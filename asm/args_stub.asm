6c46b885c8e414e6ac92d13e52942cc96618d4e4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-19 16:19:17 +0800

    args_stub.asm: replace all data to the encrypted and encoded data.

--- asm/args_stub.asm
+++ asm/args_stub.asm
@@ -2,12 +2,12 @@ IFDEF _WIN32
 .model tiny
 ENDIF
 
-; total offset of the shellcode tail is (32 + 8 + (4+8) + (4+12)) = 68 bytes
+; total offset of the shellcode tail is (32 + 8 + (4+4) + (4+12)) = 64 bytes
 
 ; +---------+----------+-----------+----------+----------+
 ; |   key   | num args | args size | arg size | arg data |
 ; +---------+----------+-----------+----------+----------+
-; | 32 byte |  uint32  |  uint32   |  uint32  |    var   |
+; | 32 byte |  uint32  |  uint32   |  uint32  |   var    |
 ; +---------+----------+-----------+----------+----------+
 
 .code
@@ -18,23 +18,32 @@ ELSE
   Argument_Stub proc
 ENDIF
 
-  db 11h, 11h, 11h, 11h  ; 32 bytes decrypt key
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
-  db 11h, 11h, 11h, 11h
+  ; 32 bytes decrypt key
+  db 0B3h, 0D9h, 0CDh, 034h
+  db 0AAh, 09Dh, 066h, 06Dh
+  db 03Eh, 08Eh, 047h, 056h
+  db 043h, 000h, 046h, 0BBh
+  db 0A2h, 014h, 03Dh, 040h
+  db 097h, 090h, 02Eh, 0F7h
+  db 032h, 008h, 0BAh, 0CCh
+  db 031h, 0FDh, 0F8h, 060h
 
-  db 02h, 00h, 00h, 00h  ; record the number of the arguments
-  db 18h, 00h, 00h, 00h  ; record the total argument data size
+  ; record the number of the arguments
+  db 002h, 000h, 000h, 000h
+  ; record the total argument data size
+  db 018h, 000h, 000h, 000h
 
-  db 04h, 00h, 00h, 00h  ; record the size of the argument-1
-  db 78h, 56h, 34h, 12h  ; argument-1 data
+  ; record the size of the argument-1
+  db 0B7h, 0D9h, 0CDh, 034h
+  ; argument-1 data
+  db 0D2h, 0CBh, 052h, 07Fh
 
-  db 09h, 00h, 00h, 00h  ; record the size of the argument-2
-  db "aaaabbbbccc", 00h  ; argument-2 data
+  ; record the size of the argument-2
+  db 032h, 08Eh, 047h, 056h
+  ; argument-2 data
+  db 022h, 061h, 027h, 0DAh
+  db 0C0h, 076h, 05Fh, 022h
+  db 0F4h, 0F3h, 04Dh, 0F7h
 
 IFDEF _WIN32
   _Argument_Stub@0 endp
