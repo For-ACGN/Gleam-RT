df52083396e305380727fc295a008282941623c7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-19 15:07:25 +0800

    args_stub.asm: improve structure about record the total argument data size.

--- asm/args_stub.asm
+++ asm/args_stub.asm
@@ -2,13 +2,13 @@ IFDEF _WIN32
 .model tiny
 ENDIF
 
-; total size(offset) is (32 + 8 * 4) = 64 bytes
+; total offset of the shellcode tail is (32 + 8 + (4+8) + (4+12)) = 68 bytes
 
-; +----------+----------+----------+----------+----------+
-; |    key   |   size   | num args | arg size | arg data |
-; +----------+----------+----------+----------+----------+
-; | 32 bytes |  uint32  |  uint32  |  uint32  |    var   |
-; +----------+----------+----------+----------+----------+
+; +---------+----------+-----------+----------+----------+
+; |   key   | num args | args size | arg size | arg data |
+; +---------+----------+-----------+----------+----------+
+; | 32 byte |  uint32  |  uint32   |  uint32  |    var   |
+; +---------+----------+-----------+----------+----------+
 
 .code
 
@@ -27,9 +27,8 @@ ENDIF
   db 11h, 11h, 11h, 11h
   db 11h, 11h, 11h, 11h
 
-  db 18h, 00h, 00h, 00h  ; total cipher data size
-
   db 02h, 00h, 00h, 00h  ; record the number of the arguments
+  db 18h, 00h, 00h, 00h  ; record the total argument data size
 
   db 04h, 00h, 00h, 00h  ; record the size of the argument-1
   db 78h, 56h, 34h, 12h  ; argument-1 data
