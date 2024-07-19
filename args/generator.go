da4c8be234ba8046bf3a82588f3990e55b424959
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-19 16:18:07 +0800

    generator.go: add dumpBytesHex for print encoded arguments.

--- args/generator.go
+++ args/generator.go
@@ -4,26 +4,65 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"strings"
 )
 
 func main() {
+	args := [][]byte{
+		{0x78, 0x56, 0x34, 0x12},
+		[]byte("aaaabbbbccc\x00"),
+	}
+	output, err := EncodeArguments(args)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dumpBytesHex(output))
+}
 
+func dumpBytesHex(b []byte) string {
+	n := len(b)
+	builder := strings.Builder{}
+	builder.Grow(len("0FFh, ")*n - len(", "))
+	buf := make([]byte, 2)
+	var counter = 0
+	for i := 0; i < n; i++ {
+		if counter == 0 {
+			builder.WriteString("  db ")
+		}
+		hex.Encode(buf, b[i:i+1])
+		builder.WriteString("0")
+		builder.Write(bytes.ToUpper(buf))
+		builder.WriteString("h")
+		counter++
+		if counter != 4 {
+			builder.WriteString(", ")
+			continue
+		}
+		builder.WriteString("\r\n")
+		counter = 0
+	}
+	return builder.String()
 }
 
 // +---------+----------+-----------+----------+----------+
 // |   key   | num args | args size | arg size | arg data |
 // +---------+----------+-----------+----------+----------+
-// | 32 byte |  uint32  |  uint32   |  uint32  |    var   |
+// | 32 byte |  uint32  |  uint32   |  uint32  |   var    |
 // +---------+----------+-----------+----------+----------+
 
-const OffsetRuntimeTail = 32 + 8 + (4 + 8) + (4 + 12)
+// OffsetRuntimeTail is the offset of runtime tail for set encoded arguments
+const OffsetRuntimeTail = 32 + 8 + (4 + 4) + (4 + 12)
 
 const (
 	cryptoKeySize  = 32
 	offsetFirstArg = 32 + 4 + 4
 )
 
+// EncodeArguments is used to encode and encrypt arguments for runtime
 func EncodeArguments(args [][]byte) ([]byte, error) {
 	key := make([]byte, cryptoKeySize)
 	_, err := rand.Read(key)
@@ -40,7 +79,7 @@ func EncodeArguments(args [][]byte) ([]byte, error) {
 	// calculate the total size of the arguments
 	var totalSize int
 	for i := 0; i < len(args); i++ {
-		totalSize += len(args[i])
+		totalSize += 4 + len(args[i])
 	}
 	binary.LittleEndian.PutUint32(b, uint32(totalSize))
 	buf.Write(b)
