0c31d8030b2ff66e0702edeadc6fc6b9e641d259
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-21 14:36:07 +0800

    test.h: refactor unit tests about runtime, add TestRuntime_Exit.

--- test/test.h
+++ test/test.h
@@ -5,15 +5,21 @@
 
 bool TestRandom();
 bool TestCrypto();
-bool TestRuntime();
+
+bool TestInitRuntime();
+bool TestRuntime_Exit();
 
 typedef bool (*test_t)();
 typedef struct { byte* Name; test_t Test; } unit;
 
 static unit tests[] = {
-    { "Random",  TestRandom },
-    { "Crypto",  TestCrypto },
-    { "Runtime", TestRuntime },
+    { "Random", TestRandom },
+    { "Crypto", TestCrypto },
+
+    { "InitRuntime",  TestInitRuntime  },
+
+
+    { "Runtime_Exit", TestRuntime_Exit },
 };
 
 #endif // TEST_H
