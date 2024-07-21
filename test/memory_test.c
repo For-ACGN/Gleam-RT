01048500a3d2dc4581c1b068e8086d2124445850
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-21 14:56:21 +0800

    memory_test.c: replace return value -1 to false in some tests.

--- test/memory_test.c
+++ test/memory_test.c
@@ -9,92 +9,90 @@
     uint64* test1 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test1 == NULL)
     {
-        return -1;
+        return false;
     }
     *test1 = 0x1234567812345600;
 
     uint64* test2 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test2 == NULL)
     {
-        return -1;
+        return false;
     }
     *test2 = 0x1234567812345601;
 
     uint64* test3 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test3 == NULL)
     {
-        return -1;
+        return false;
     }
     *test3 = 0x1234567812345602;
 
-    errno errno;
-
-    errno = runtime->Hide();
+    errno errno = runtime->Hide();
     if (errno != NO_ERROR)
     {
-        printf_s("error: %X\n", errno);
+        printf_s("failed to hide: 0x%lX\n", errno);
         return false;
     }
     errno = runtime->Recover();
     if (errno != NO_ERROR)
     {
-        printf_s("error: %X\n", errno);
+        printf_s("failed to recover: 0x%lX\n", errno);
         return false;
     }
 
     uint64* test4 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test4 == NULL)
     {
-        return -1;
+        return false;
     }
     *test4 = 0x1234567812345603;
 
     if (!runtime->MemFree(test3))
     {
-        return -1;
+        return false;
     }
     if (!runtime->MemFree(test1))
     {
-        return -1;
+        return false;
     }
 
     uint64* test5 = (uint64*)runtime->MemAlloc(sizeof(uint64));
     if (test5 == NULL)
     {
-        return -1;
+        return false;
     }
     *test5 = 0x1234567812345600;
 
     errno = runtime->Hide();
     if (errno != NO_ERROR)
     {
-        printf_s("error: %X\n", errno);
+        printf_s("failed to hide: 0x%lX\n", errno);
         return false;
     }
     errno = runtime->Recover();
     if (errno != NO_ERROR)
     {
-        printf_s("error: %X\n", errno);
+        printf_s("failed to recover: 0x%lX\n", errno);
         return false;
     }
     errno = runtime->SleepHR(1000);
     if (errno != NO_ERROR)
     {
-        printf_s("error: %X\n", errno);
+        printf_s("failed to call SleepHR: 0x%lX\n", errno);
         return false;
     }
 
     if (!runtime->MemFree(test2))
     {
-        return -1;
+        return false;
     }
     if (!runtime->MemFree(test4))
     {
-        return -1;
+        return false;
     }
     if (!runtime->MemFree(test5))
     {
-        return -1;
+        return false;
     }
 
     return true;
