53636ca8d273a9cb8353d81eebb6b6a12c688df9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-22 13:54:42 +0800

    lib_string.c: add strcmp_a and strcmp_w for compare ASCII and Unicode string.

--- src/lib_string.c
+++ src/lib_string.c
@@ -41,4 +41,57 @@ uint strlen_w(utf16 s)
     return l;
 }
 
+__declspec(noinline)
+int strcmp_a(ascii a, ascii b)
+{
+    for (;;)
+    {
+        byte s0 = *a;
+        byte s1 = *b;
+        if (s0 == s1)
+        {
+            if (s0 == 0x00)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+__declspec(noinline)
+int strcmp_w(utf16 a, utf16 b)
+{
+    for (;;)
+    {
+        uint16 s0 = *a;
+        uint16 s1 = *b;
+        if (s0 == s1)
+        {
+            if (s0 == 0x0000)
+            {
+                return 0;
+            }
+            a++;
+            b++;
+            continue;
+        }
+        if (s0 > s1)
+        {
+            return 1;
+        } else
+        {
+            return -1;
+        }
+    }
+}
+
 #pragma optimize("", on)
