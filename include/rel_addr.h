8d4726b4355f830d9a0af4fa5f689423395600c5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-26 17:06:27 +0800

    rel_addr.h: add new module for calculate function address with offset.

---
+++ include/rel_addr.h
@@ -0,0 +1,18 @@
+#ifndef REL_ADDR_H
+#define REL_ADDR_H
+
+// x64 can obtain the absolute address of a function through
+// RIP relative addressing without external tool modification.
+// However, on x86, since "mov reg, abs_addr" is always used
+// to obtain function addresses, external tools & inline assembly 
+// techniques are required to implement relative addressing.
+// 
+// The variable func will be replace to the offset of the GetFuncAddr
+
+#ifdef _WIN64
+	#define GetFuncAddr(func)(func)
+#elif _WIN32
+	void* GetFuncAddr(void* func);
+#endif
+
+#endif // REL_ADDR_H
