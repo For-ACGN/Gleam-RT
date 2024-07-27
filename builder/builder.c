621a201d1fd90da2910d9951dde4c7e68e7ab8ce
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-27 13:35:47 +0800

    builder.c: refactor runtime shellcode builder, update fixFuncOffset.

--- builder/builder.c
+++ builder/builder.c
@@ -3,8 +3,14 @@
 #include "runtime.h"
 #include "epilogue.h"
 
+void fixFuncOffset();
+
 int __cdecl main()
 {
+    // fix offset about function address
+    fixFuncOffset();
+
+    // create file for save shellcode 
 #ifdef _WIN64
     FILE* file = fopen("../dist/GleamRT_x64.bin", "wb");
 #elif _WIN32
@@ -15,7 +21,6 @@ int __cdecl main()
         printf_s("failed to open file");
         return 1;
     }
-
     uintptr begin = (uintptr)(&InitRuntime);
     uintptr end   = (uintptr)(&Epilogue);
     uintptr size  = end - begin;
@@ -27,9 +32,21 @@ int __cdecl main()
     }
     fclose(file);
 
+    // test shellcode
     Runtime_M* RuntimeM = InitRuntime(NULL);
     printf_s("RuntimeM: 0x%llX\n", (uint64)RuntimeM);
-
+    if (RuntimeM == NULL)
+    {
+        printf_s("failed to test shellcode");
+        return 3;
+    }
     printf_s("save shellcode successfully");
     return 0;
+}
+
+void fixFuncOffset()
+{
+#ifndef _WIN64
+
+#endif
 }
