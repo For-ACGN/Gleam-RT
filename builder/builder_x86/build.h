5005c29c208a54c3a92578610f74b6132e68baac
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-29 14:09:29 +0800

    builder_x86: move original builder to builder_x86 for build x86 runtime shellcode.

---
+++ builder/builder_x86/build.h
@@ -0,0 +1,10 @@
+#ifndef BUILD_H
+#define BUILD_H
+
+// RELEASE_MODE: remove debug modules for generate shellcode
+// FAST_CRYPTO:  replace original algorithm to xor for test
+
+#define RELEASE_MODE
+// #define FAST_CRYPTO
+
+#endif // BUILD_H
