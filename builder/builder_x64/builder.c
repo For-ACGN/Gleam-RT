3ba1cb8ad0b10764aa15154d8c2ec619c3872ebf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-07-30 15:53:00 +0800

    builder_x64/builder.c: remove code about generate shellcode for x86.

--- builder/builder_x64/builder.c
+++ builder/builder_x64/builder.c
@@ -7,43 +7,16 @@
 #include "rel_addr.h"
 #include "epilogue.h"
 
-#define FUNC_OFFSET_RUNTIME (\
-    1 + 2 + 4 + 4 + 2 + 1 + 5 +\
-    5 + 2 + 2 + 2 + 3 + 3 + 4 +\
-    (2 + 1)\
-)
-#define FUNC_OFFSET_LIBRARY  (6 + 5 + 1 + 2)
-#define FUNC_OFFSET_MEMORY   (4 + 8 + 1 + 2)
-#define FUNC_OFFSET_THREAD   (7 + 7 + 1 + 2)
-#define FUNC_OFFSET_RESOURCE (8 + 5 + 1 + 2)
-#define FUNC_OFFSET_ARGUMENT (3 + 5 + 1 + 1 + 2)
-
-#define FUNC_OFFSET_COUNTER (\
-    FUNC_OFFSET_RUNTIME  + FUNC_OFFSET_LIBRARY +\
-    FUNC_OFFSET_MEMORY   + FUNC_OFFSET_THREAD  +\
-    FUNC_OFFSET_RESOURCE + FUNC_OFFSET_ARGUMENT \
-)
-
-int fixFuncOffset();
 int saveShellcode();
 int testShellcode();
 
-int __cdecl main()
+int main()
 {
-    int ret;
-    // fix offset about function address
-    ret = fixFuncOffset();
+    int ret = saveShellcode();
     if (ret != 0)
     {
         return ret;
     }
-    // create file for save shellcode 
-    ret = saveShellcode();
-    if (ret != 0)
-    {
-        return ret;
-    }
-    // test initialize runtime
     ret = testShellcode();
     if (ret != 0)
     {
@@ -53,71 +26,13 @@ int __cdecl main()
     return 0;
 }
 
-int fixFuncOffset()
-{
-#ifndef _WIN64
-    uintptr stub  = (uintptr)(&GetFuncAddr);
-    uintptr begin = (uintptr)(&InitRuntime);
-    uintptr end   = (uintptr)(&Epilogue);
-    // adjust memory page protect
-    VirtualProtect_t VirtualProtect = FindAPI(0xB4365E5D, 0x46833E39);
-    SIZE_T size = end - begin;
-    DWORD old;
-    if (!VirtualProtect(&InitRuntime, size, PAGE_EXECUTE_READWRITE, &old))
-    {
-        return 1;
-    }
-    // search the instructions about "call GetFuncAddr"
-    uint counter = 0;
-    for (uintptr eip = begin; eip < end; eip++)
-    {
-        // instruction about call
-        if (*(byte*)eip != 0xE8)
-        {
-            continue;
-        }
-        // EIP + call rel + len(call)
-        if (eip + *(uint32*)(eip + 1) + 5 != stub)
-        {
-            continue;
-        }
-        // search the instruction that store function absolute address
-        uintptr target = eip - 4;
-        for (int offset = 0; offset < 128; offset++)
-        {
-            uintptr addr = target - offset;
-            uintptr func = *(uintptr*)addr;
-            if (func < begin || func > end)
-            {
-                continue;
-            }
-            // replace the absolute address to relative address
-            *(uintptr*)addr = func - stub;
-            counter++;
-            break;
-        }
-    }
-    printf_s("total fix: %zu\n", counter);
-    if (counter != FUNC_OFFSET_COUNTER)
-    {
-        printf_s("invalid fix counter\n");
-        return 2;
-    }
-#endif
-    return 0;
-}
-
 int saveShellcode()
 {
-#ifdef _WIN64
     FILE* file = fopen("../../dist/GleamRT_x64.bin", "wb");
-#elif _WIN32
-    FILE* file = fopen("../../dist/GleamRT_x86.bin", "wb");
-#endif
     if (file == NULL)
     {
-        printf_s("failed to open file");
-        return 3;
+        printf_s("failed to create output file");
+        return 1;
     }
     uintptr begin = (uintptr)(&InitRuntime);
     uintptr end   = (uintptr)(&Epilogue);
@@ -126,7 +41,7 @@ int saveShellcode()
     if (n != 1)
     {
         printf_s("failed to save shellcode");
-        return 4;
+        return 2;
     }
     fclose(file);
     return 0;
@@ -141,7 +56,7 @@ int testShellcode()
     if (RuntimeM == NULL)
     {
         printf_s("failed to test shellcode: 0x%lX\n", GetLastErrno());
-        return 5;
+        return 3;
     }
     printf_s("RuntimeM: 0x%llX\n", (uint64)RuntimeM);
     return 0;
