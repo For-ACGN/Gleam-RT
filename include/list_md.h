f3f0162570180bdad64e6a81707afeb52899f998
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-18 14:04:12 +0800

    list_md.h: add List_Set for set element by index, improve comments.

--- include/list_md.h
+++ include/list_md.h
@@ -27,19 +27,24 @@ typedef struct {
 // List_Init is used to initialize a mesh dynamic list.
 void List_Init(List* list, List_Ctx* ctx, uint unit);
 
+// List_Set is used to set element by index.
+// If index > cap, it will resize automatically.
+bool List_Set(List* list, uint index, void* data);
+
+// List_Get is used to get element by index.
+// If index > cap, it will return NULL.
+void* List_Get(List* list, uint index);
+
 // List_Insert is used to insert a element to list.
 bool List_Insert(List* list, void* data);
 
 // List_Delete is used to delete element by index.
 bool List_Delete(List* list, uint index);
 
-// List_Get is used to get element by index.
-void* List_Get(List* list, uint index);
-
 // List_Find is used to find data and return index.
-// equal is used compare a part of data, if equal
-// is zero, it will compare whole data.
-// if index is NULL, it will not return the element index.
+// Equal is used compare a part of data, if equal is zero,
+// it will compare whole data.
+// If index is NULL, it will not return the element index.
 bool List_Find(List* list, void* data, uint equal, uint* index);
 
 // List_Resize is used to resize list buffer size.
