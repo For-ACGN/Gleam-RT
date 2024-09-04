81370447b7896f715ab16f903910efc22b48eee3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-04 16:01:26 +0800

    argument_test.c: add sub tests for get empty argument with index 2.

--- test/argument_test.c
+++ test/argument_test.c
@@ -32,7 +32,7 @@ bool TestRuntime_Argument()
 
 static bool TestArgument_GetValue()
 {
-    // get argument 0 pointer with size
+    // get argument 0 value with size
     uint32 arg0 = 0;
     uint32 size = 0;
     if (!runtime->GetArgValue(0, &arg0, &size))
@@ -52,7 +52,7 @@ static bool TestArgument_GetValue()
     }
     printf_s("arg0: 0x%X, size: %d\n", arg0, size);
 
-    // get argument 1 pointer with size
+    // get argument 1 value with size
     byte arg1[12+1];
     if (!runtime->GetArgValue(1, &arg1, &size))
     {
@@ -72,6 +72,23 @@ static bool TestArgument_GetValue()
     }
     printf_s("arg1: %s, size: %d\n", arg1, size);
 
+    // get argument 2 value with size
+    byte arg2 = 123;
+    if (!runtime->GetArgValue(2, &arg2, &size))
+    {
+        printf_s("failed to get argument 2\n");
+        return false;
+    }
+    if (arg2 != 123)
+    {
+        printf_s("argument 2 is invalid data\n");
+    }
+    if (size != 0)
+    {
+        printf_s("argument 2 size is invalid\n");
+        return false;
+    }
+
     // not receive argument size
     arg0 = 0;
     if (!runtime->GetArgValue(0, &arg0, NULL))
@@ -87,7 +104,7 @@ static bool TestArgument_GetValue()
     printf_s("arg0: 0x%X\n", arg0);
 
     // invalid index
-    if (runtime->GetArgValue(2, &arg0, NULL))
+    if (runtime->GetArgValue(3, &arg0, NULL))
     {
         printf_s("get argument with invalid index\n");
         return false;
@@ -136,6 +153,23 @@ static bool TestArgument_GetPointer()
     }
     printf_s("arg1: %s, size: %d\n", arg1, size);
 
+    // get argument 2 pointer with size
+    byte* arg2 = (byte*)123;
+    if (!runtime->GetArgPointer(2, &arg2, &size))
+    {
+        printf_s("failed to get argument 2\n");
+        return false;
+    }
+    if (arg2 != NULL)
+    {
+        printf_s("argument 2 is invalid data\n");
+    }
+    if (size != 0)
+    {
+        printf_s("argument 2 size is invalid\n");
+        return false;
+    }
+
     // not receive argument size
     arg0 = NULL;
     if (!runtime->GetArgPointer(0, &arg0, NULL))
@@ -151,7 +185,7 @@ static bool TestArgument_GetPointer()
     printf_s("arg0: 0x%X\n", *arg0);
 
     // invalid index
-    if (runtime->GetArgPointer(2, &arg0, NULL))
+    if (runtime->GetArgPointer(3, &arg0, NULL))
     {
         printf_s("get argument with invalid index\n");
         return false;
@@ -163,10 +197,10 @@ static bool TestArgument_Erase()
 {
     if (!runtime->EraseArgument(1))
     {
-        printf_s("failed to earse argument 1\n");
+        printf_s("failed to erase argument 1\n");
         return false;
     }
-    printf_s("earse argument 1\n");
+    printf_s("erase argument 1\n");
 
     byte*  arg1 = NULL;
     uint32 size = 0;
@@ -192,7 +226,7 @@ static bool TestArgument_Erase()
 static bool TestArgument_EraseAll()
 {
     runtime->EraseAllArgs();
-    printf_s("earse all arguments\n");
+    printf_s("erase all arguments\n");
 
     uint32* arg0 = NULL;
     uint32  size = 0;
