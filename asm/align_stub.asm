daeef5b9eb23981386a63136b221fbc2949a2546
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-10 15:46:58 +0800

    align_stub.asm: add Align_Stub for align instructions manually.

---
+++ asm/align_stub.asm
@@ -0,0 +1,19 @@
+IFDEF _WIN32
+.model tiny
+ENDIF
+
+.code
+
+IFDEF _WIN32
+  _Align_Stub@0 proc
+  db 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh
+  db 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh
+  _Align_Stub@0 endp
+ELSE
+  Align_Stub proc
+  db 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh
+  db 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh
+  Align_Stub endp
+ENDIF
+
+end
