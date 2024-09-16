d946b686c40d4182383ac5d11fd3403ad72a4dab
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-16 14:43:43 +0800

    crypto.c: remove negBit and swapBit, finish encryptBuf and decryptBuf.

--- src/crypto.c
+++ src/crypto.c
@@ -21,8 +21,6 @@ static void encryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox);
 static void decryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox);
 static void initSBox(byte* sBox, byte* key, byte* iv);
 static void permuteSBox(byte* sBox);
-static byte negBit(byte b, uint8 n);
-static byte swapBit(byte b, uint8 p1, uint8 p2);
 static byte ror(byte value, uint8 bits);
 static byte rol(byte value, uint8 bits);
 
@@ -116,23 +114,14 @@ static void encryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
         b6 ^= seed6;
         b7 ^= seed7;
 
-        b0 = negBit(b0, (seed0>>8) % 8);
-        b1 = negBit(b1, (seed1>>8) % 8);
-        b2 = negBit(b2, (seed2>>8) % 8);
-        b3 = negBit(b3, (seed3>>8) % 8);
-        b4 = negBit(b4, (seed4>>8) % 8);
-        b5 = negBit(b5, (seed5>>8) % 8);
-        b6 = negBit(b6, (seed6>>8) % 8);
-        b7 = negBit(b7, (seed7>>8) % 8);
-
-        b0 = swapBit(b0, (seed0 >> 16) % 8, (seed0 + 1) % 8);
-        b1 = swapBit(b1, (seed1 >> 16) % 8, (seed1 + 1) % 8);
-        b2 = swapBit(b2, (seed2 >> 16) % 8, (seed2 + 1) % 8);
-        b3 = swapBit(b3, (seed3 >> 16) % 8, (seed3 + 1) % 8);
-        b4 = swapBit(b4, (seed4 >> 16) % 8, (seed4 + 1) % 8);
-        b5 = swapBit(b5, (seed5 >> 16) % 8, (seed5 + 1) % 8);
-        b6 = swapBit(b6, (seed6 >> 16) % 8, (seed6 + 1) % 8);
-        b7 = swapBit(b7, (seed7 >> 16) % 8, (seed7 + 1) % 8);
+        b0 = ror(b0, (seed0 >> 8) % 8);
+        b1 = ror(b1, (seed1 >> 8) % 8);
+        b2 = ror(b2, (seed2 >> 8) % 8);
+        b3 = ror(b3, (seed3 >> 8) % 8);
+        b4 = ror(b4, (seed4 >> 8) % 8);
+        b5 = ror(b5, (seed5 >> 8) % 8);
+        b6 = ror(b6, (seed6 >> 8) % 8);
+        b7 = ror(b7, (seed7 >> 8) % 8);
 
         b0 ^= seed0 >> 8;
         b1 ^= seed1 >> 8;
@@ -143,14 +132,14 @@ static void encryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
         b6 ^= seed6 >> 8;
         b7 ^= seed7 >> 8;
 
-        b0 = ror(b0, (seed0 >> 24) % 8);
-        b1 = ror(b1, (seed1 >> 24) % 8);
-        b2 = ror(b2, (seed2 >> 24) % 8);
-        b3 = ror(b3, (seed3 >> 24) % 8);
-        b4 = ror(b4, (seed4 >> 24) % 8);
-        b5 = ror(b5, (seed5 >> 24) % 8);
-        b6 = ror(b6, (seed6 >> 24) % 8);
-        b7 = ror(b7, (seed7 >> 24) % 8);
+        b0 = ror(b0, (seed0 >> 16) % 8);
+        b1 = ror(b1, (seed1 >> 16) % 8);
+        b2 = ror(b2, (seed2 >> 16) % 8);
+        b3 = ror(b3, (seed3 >> 16) % 8);
+        b4 = ror(b4, (seed4 >> 16) % 8);
+        b5 = ror(b5, (seed5 >> 16) % 8);
+        b6 = ror(b6, (seed6 >> 16) % 8);
+        b7 = ror(b7, (seed7 >> 16) % 8);
 
         b0 ^= (seed0 >> 16);
         b1 ^= (seed1 >> 16);
@@ -160,6 +149,24 @@ static void encryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
         b5 ^= (seed5 >> 16);
         b6 ^= (seed6 >> 16);
         b7 ^= (seed7 >> 16);
+
+        b0 = ror(b0, (seed0 >> 24) % 8);
+        b1 = ror(b1, (seed1 >> 24) % 8);
+        b2 = ror(b2, (seed2 >> 24) % 8);
+        b3 = ror(b3, (seed3 >> 24) % 8);
+        b4 = ror(b4, (seed4 >> 24) % 8);
+        b5 = ror(b5, (seed5 >> 24) % 8);
+        b6 = ror(b6, (seed6 >> 24) % 8);
+        b7 = ror(b7, (seed7 >> 24) % 8);
+
+        b0 ^= (seed0 >> 24);
+        b1 ^= (seed1 >> 24);
+        b2 ^= (seed2 >> 24);
+        b3 ^= (seed3 >> 24);
+        b4 ^= (seed4 >> 24);
+        b5 ^= (seed5 >> 24);
+        b6 ^= (seed6 >> 24);
+        b7 ^= (seed7 >> 24);
 
         // permutation
         b0 = sBox[b0];
@@ -213,10 +220,52 @@ void DecryptBuf(byte* buf, uint size, byte* key, byte* iv)
 
 static void decryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
 {
-    uintptr i  = 0;
+    // just generate from random data
+    uint32 seeds[8] = {
+        0xFA1C345C, 0xAF16C47C, 0x1C553A02, 0x9EDAC545,
+        0xC942A49C, 0x5FE323EC, 0x9A1934AC, 0x2CB443C1,
+    };
+    // initialize random seeds
+    for (int i = 0; i < 8; i++)
+    {
+        seeds[i] *= (uint32)(key[i * 4 + 0]) << 0;
+        seeds[i] *= (uint32)(key[i * 4 + 1]) << 8;
+        seeds[i] *= (uint32)(key[i * 4 + 2]) << 16;
+        seeds[i] *= (uint32)(key[i * 4 + 3]) << 24;
+
+        seeds[i] ^= (uint32)(key[i * 4 + 0]) << 0;
+        seeds[i] ^= (uint32)(key[i * 4 + 1]) << 8;
+        seeds[i] ^= (uint32)(key[i * 4 + 2]) << 16;
+        seeds[i] ^= (uint32)(key[i * 4 + 3]) << 24;
+
+        seeds[i] ^= (uint32)(iv[i * 2 + 0]) << 8;
+        seeds[i] ^= (uint32)(iv[i * 2 + 1]) << 24;
+    }
+
+    // load random seeds
+    uint32 seed0 = seeds[0];
+    uint32 seed1 = seeds[1];
+    uint32 seed2 = seeds[2];
+    uint32 seed3 = seeds[3];
+    uint32 seed4 = seeds[4];
+    uint32 seed5 = seeds[5];
+    uint32 seed6 = seeds[6];
+    uint32 seed7 = seeds[7];
+
+    uintptr i = 0;
     uint limit = size - (size % PARALLEL_LEVEL);
     for (; i < limit; i += PARALLEL_LEVEL)
     {
+        // update seeds
+        seed0 = XORShift32(seed0);
+        seed1 = XORShift32(seed1);
+        seed2 = XORShift32(seed2);
+        seed3 = XORShift32(seed3);
+        seed4 = XORShift32(seed4);
+        seed5 = XORShift32(seed5);
+        seed6 = XORShift32(seed6);
+        seed7 = XORShift32(seed7);
+
         // load cipher data
         byte b0 = buf[i + 0];
         byte b1 = buf[i + 1];
@@ -227,6 +276,78 @@ static void decryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
         byte b6 = buf[i + 6];
         byte b7 = buf[i + 7];
 
+        // permutation
+        b0 = sBox[b0];
+        b1 = sBox[b1];
+        b2 = sBox[b2];
+        b3 = sBox[b3];
+        b4 = sBox[b4];
+        b5 = sBox[b5];
+        b6 = sBox[b6];
+        b7 = sBox[b7];
+
+        b0 ^= (seed0 >> 24);
+        b1 ^= (seed1 >> 24);
+        b2 ^= (seed2 >> 24);
+        b3 ^= (seed3 >> 24);
+        b4 ^= (seed4 >> 24);
+        b5 ^= (seed5 >> 24);
+        b6 ^= (seed6 >> 24);
+        b7 ^= (seed7 >> 24);
+
+        b0 = rol(b0, (seed0 >> 24) % 8);
+        b1 = rol(b1, (seed1 >> 24) % 8);
+        b2 = rol(b2, (seed2 >> 24) % 8);
+        b3 = rol(b3, (seed3 >> 24) % 8);
+        b4 = rol(b4, (seed4 >> 24) % 8);
+        b5 = rol(b5, (seed5 >> 24) % 8);
+        b6 = rol(b6, (seed6 >> 24) % 8);
+        b7 = rol(b7, (seed7 >> 24) % 8);
+
+        b0 ^= (seed0 >> 16);
+        b1 ^= (seed1 >> 16);
+        b2 ^= (seed2 >> 16);
+        b3 ^= (seed3 >> 16);
+        b4 ^= (seed4 >> 16);
+        b5 ^= (seed5 >> 16);
+        b6 ^= (seed6 >> 16);
+        b7 ^= (seed7 >> 16);
+
+        b0 = rol(b0, (seed0 >> 16) % 8);
+        b1 = rol(b1, (seed1 >> 16) % 8);
+        b2 = rol(b2, (seed2 >> 16) % 8);
+        b3 = rol(b3, (seed3 >> 16) % 8);
+        b4 = rol(b4, (seed4 >> 16) % 8);
+        b5 = rol(b5, (seed5 >> 16) % 8);
+        b6 = rol(b6, (seed6 >> 16) % 8);
+        b7 = rol(b7, (seed7 >> 16) % 8);
+
+        b0 ^= seed0 >> 8;
+        b1 ^= seed1 >> 8;
+        b2 ^= seed2 >> 8;
+        b3 ^= seed3 >> 8;
+        b4 ^= seed4 >> 8;
+        b5 ^= seed5 >> 8;
+        b6 ^= seed6 >> 8;
+        b7 ^= seed7 >> 8;
+
+        b0 = rol(b0, (seed0 >> 8) % 8);
+        b1 = rol(b1, (seed1 >> 8) % 8);
+        b2 = rol(b2, (seed2 >> 8) % 8);
+        b3 = rol(b3, (seed3 >> 8) % 8);
+        b4 = rol(b4, (seed4 >> 8) % 8);
+        b5 = rol(b5, (seed5 >> 8) % 8);
+        b6 = rol(b6, (seed6 >> 8) % 8);
+        b7 = rol(b7, (seed7 >> 8) % 8);
+
+        b0 ^= seed0;
+        b1 ^= seed1;
+        b2 ^= seed2;
+        b3 ^= seed3;
+        b4 ^= seed4;
+        b5 ^= seed5;
+        b6 ^= seed6;
+        b7 ^= seed7;
 
         // permutation
         b0 = sBox[b0];
@@ -248,67 +369,21 @@ static void decryptBuf(byte* buf, uint size, byte* key, byte* iv, byte* sBox)
         buf[i + 6] = b6;
         buf[i + 7] = b7;
     }
+
+    // update random seeds
+    seeds[0] = seed0;
+    seeds[1] = seed1;
+    seeds[2] = seed2;
+    seeds[3] = seed3;
+    seeds[4] = seed4;
+    seeds[5] = seed5;
+    seeds[6] = seed6;
+    seeds[7] = seed7;
+
     // process remaining not aligned data
     for (; i < size; i++)
     {
         buf[i] = sBox[buf[i]];
-    }
-
-    return;
-
-
-
-
-    // initialize status
-    uint kIdx = 0;
-    byte dCtr = 0;
-    uint bCtr = 0;
-    byte last = 1;
-    byte cKey;
-    byte data;
-    // decrypt buffer
-    for (uintptr i = 0; i < size; i++)
-    {
-        // update current key byte
-        cKey = *(key + kIdx);
-
-        // read byte from buffer
-        data = *(buf + i);
-
-        data = sBox[data];
-        data = rol(data, cKey % 8);
-        data = swapBit(data, last % 8, cKey % 8);
-        data = negBit(data, cKey % 8);
-        data ^= cKey;
-
-        data = sBox[data];
-        data = rol(data, last % 8);
-        data = swapBit(data, last % 8, cKey % 8);
-        data = negBit(data, last % 8);
-        data ^= last;
-
-        data = sBox[data];
-        data = rol(data, dCtr % 8);
-        data = swapBit(data, dCtr % 8, cKey % 8);
-        data = negBit(data, dCtr % 8);
-        data ^= dCtr;
-
-        // update last byte
-        last = *(buf + i);
-
-        // write byte to the buffer
-        *(buf + i) = data;
-
-        // update key index
-        kIdx++;
-        if (kIdx >= CRYPTO_KEY_SIZE)
-        {
-            kIdx = 0;
-        }
-
-        // update counter
-        dCtr += (kIdx + cKey + last) % 16;
-        bCtr += (kIdx + cKey + last) % 32;
     }
 }
 
@@ -352,26 +427,6 @@ static void permuteSBox(byte* sBox)
     {
         sBox[buf[i]] = (byte)i;
     }
-}
-
-static byte negBit(byte b, uint8 n)
-{
-    return b ^ (1 << n);
-}
-
-static byte swapBit(byte b, uint8 p1, uint8 p2)
-{
-    // extract the bits at pos1 and pos2
-    byte bit1 = (b >> p1) & 1;
-    byte bit2 = (b >> p2) & 1;
-    if (bit1 == bit2)
-    {
-        return b;
-    }
-    // use XOR to flip the bits
-    b ^= (1 << p1);
-    b ^= (1 << p2);
-    return b;
 }
 
 static byte ror(byte value, uint8 bits)
