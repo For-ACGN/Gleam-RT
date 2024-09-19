18b26859be420df0251d4b65b270072cc6ae9758
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-19 14:10:02 +0800

    check environment variable VisualStudio is set, clean dist directory before extract.

--- Build.bat
+++ Build.bat
@@ -1,6 +1,12 @@
 @echo off
 
 echo ========== initialize Visual Studio environment ==========
+
+if "%VisualStudio%" == "" (
+    echo environment variable "VisualStudio" is not set
+    exit /b 1
+)
+
 call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
 
 echo ================= clean builder old files ================
@@ -10,16 +16,20 @@ rd /S /Q "Release"
 rd /S /Q "x64"
 
 echo ==================== generate builder ====================
-MSbuild Gleam-RT.sln /t:builder /p:Configuration=Release /p:Platform=x64
-MSbuild Gleam-RT.sln /t:builder /p:Configuration=Release /p:Platform=x86
+MSBuild.exe Gleam-RT.sln /t:builder /p:Configuration=Release /p:Platform=x64
+MSBuild.exe Gleam-RT.sln /t:builder /p:Configuration=Release /p:Platform=x86
 
 echo ================ extract runtime shellcode ===============
+del /S /Q dist
 cd builder
+echo --------extract shellcode for x64--------
 "..\x64\Release\builder.exe"
+echo --------extract shellcode for x86--------
 "..\Release\builder.exe"
+cd ..
 
 echo ================ generate assembly module ================
-cd ..\script
+cd script
 call asm_mod.bat
 cd ..
 
