d145112236b1a2c87070198806aca288a80ecae7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-26 16:24:24 +0800

    lib_memory.h: adjust functions order, improve argument name.

--- include/lib_memory.h
+++ include/lib_memory.h
@@ -4,22 +4,23 @@
 #include "c_types.h"
 
 typedef void* (*malloc_t)(uint size);
-typedef void* (*realloc_t)(void* address, uint size);
-typedef bool  (*free_t)(void* address);
-
-// mem_equal is used to compare the memory is equal.
-bool mem_equal(void* dst, void* src, uint size);
-
-// mem_zero is used to check the destination memory are all zero.
-bool mem_zero(void* dst, uint size);
+typedef void* (*calloc_t)(uint num, uint size);
+typedef void* (*realloc_t)(void* ptr, uint size);
+typedef bool  (*free_t)(void* ptr);
 
 // mem_copy is used to copy source memory data to the destination.
 void mem_copy(void* dst, void* src, uint size);
 
+// mem_init is used to fill the memory with zero.
+void mem_init(void* ptr, uint num);
+
 // mem_set is used to fill the memory with value.
 void mem_set(void* ptr, byte val, uint num);
 
-// mem_clean is used to fill the memory with zero.
-void mem_clean(void* ptr, uint num);
+// mem_equal is used to compare the memory is equal.
+bool mem_equal(void* dst, void* src, uint size);
+
+// mem_zero is used to check the memory are all zero.
+bool mem_is_zero(void* ptr, uint size);
 
 #endif // LIB_MEMORY_H
