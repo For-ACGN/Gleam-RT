7b4d6c2a3bc785cb814576f5ddb79274b8e5a07d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-28 14:20:07 +0800

    runtime.h: adjust methods order about basic modules, update comment.

--- include/runtime.h
+++ include/runtime.h
@@ -15,29 +15,17 @@
 #define OPT_OFFSET_NOT_TRACK_CURRENT_THREAD 3
 
 // for generic shellcode development.
-typedef void (*Sleep_t)(uint32 milliseconds);
-
-typedef void   (*RandBuf_t)(byte* buf, int64 size);
-typedef bool   (*RandBool_t)(uint64 seed);
-typedef int64  (*RandInt64_t)(uint64 seed);
-typedef uint64 (*RandUint64_t)(uint64 seed);
-typedef int64  (*RandInt64N_t)(uint64 seed, int64 n);
-typedef uint64 (*RandUint64N_t)(uint64 seed, uint64 n);
-
-typedef void (*Encrypt_t)(byte* buf, uint size, byte* key, byte* iv);
-typedef void (*Decrypt_t)(byte* buf, uint size, byte* key, byte* iv);
-
-typedef uint (*Compress_t)(void* dst, void* src);
-typedef uint (*Decompress_t)(void* dst, void* src);
-
 typedef void* (*MemAlloc_t)(uint size);
 typedef void* (*MemCalloc_t)(uint num, uint size);
 typedef void* (*MemRealloc_t)(void* ptr, uint size);
 typedef void  (*MemFree_t)(void* ptr);
 
+// about thread module
 typedef HANDLE (*ThdNew_t)(void* address, void* parameter, bool track);
 typedef void   (*ThdExit_t)();
+typedef void   (*Sleep_t)(uint32 milliseconds);
 
+// about argument store
 typedef bool (*GetArgValue_t)(uint index, void* value, uint32* size);
 typedef bool (*GetArgPointer_t)(uint index, void** pointer, uint32* size);
 typedef bool (*EraseArgument_t)(uint index);
@@ -51,6 +39,22 @@ typedef void (*EraseAllArgs_t)();
 typedef void* (*GetProcAddressByName_t)(HMODULE hModule, LPCSTR lpProcName, bool hook);
 typedef void* (*GetProcAddressByHash_t)(uint hash, uint key, bool hook);
 typedef void* (*GetProcAddressOriginal_t)(HMODULE hModule, LPCSTR lpProcName);
+
+// about random module
+typedef void   (*RandBuf_t)(byte* buf, int64 size);
+typedef bool   (*RandBool_t)(uint64 seed);
+typedef int64  (*RandInt64_t)(uint64 seed);
+typedef uint64 (*RandUint64_t)(uint64 seed);
+typedef int64  (*RandInt64N_t)(uint64 seed, int64 n);
+typedef uint64 (*RandUint64N_t)(uint64 seed, uint64 n);
+
+// about crypto module
+typedef void (*Encrypt_t)(byte* buf, uint size, byte* key, byte* iv);
+typedef void (*Decrypt_t)(byte* buf, uint size, byte* key, byte* iv);
+
+// about compress module
+typedef uint (*Compress_t)(void* dst, void* src);
+typedef uint (*Decompress_t)(void* dst, void* src);
 
 // runtime core methods
 // it is NOT recommended use "Hide" and "Recover", these functions
@@ -78,27 +82,10 @@ typedef struct {
 
 // Runtime_M contains exported runtime methods.
 typedef struct {
-    // generic module
+    // about hash api
     FindAPI_t   FindAPI;
     FindAPI_A_t FindAPI_A;
     FindAPI_W_t FindAPI_W;
-    Sleep_t     Sleep;
-
-    // random module
-    RandBuf_t     RandBuf;
-    RandBool_t    RandBool;
-    RandInt64_t   RandInt64;
-    RandUint64_t  RandUint64;
-    RandInt64N_t  RandInt64N;
-    RandUint64N_t RandUint64N;
-
-    // crypto module
-    Encrypt_t Encrypt;
-    Decrypt_t Decrypt;
-
-    // compress module
-    Compress_t   Compress;
-    Decompress_t Decompress;
 
     // library tracker
     LoadLibraryA_t   LoadLibraryA;
@@ -117,6 +104,7 @@ typedef struct {
     // thread tracker
     ThdNew_t  NewThread;
     ThdExit_t ExitThread;
+    Sleep_t   Sleep;
 
     // argument store
     GetArgValue_t   GetArgValue;
@@ -129,7 +117,23 @@ typedef struct {
     GetProcAddressByHash_t   GetProcAddressByHash;
     GetProcAddressOriginal_t GetProcAddressOriginal;
     
-    // runtime core
+    // random module
+    RandBuf_t     RandBuf;
+    RandBool_t    RandBool;
+    RandInt64_t   RandInt64;
+    RandUint64_t  RandUint64;
+    RandInt64N_t  RandInt64N;
+    RandUint64N_t RandUint64N;
+
+    // crypto module
+    Encrypt_t Encrypt;
+    Decrypt_t Decrypt;
+
+    // compress module
+    Compress_t   Compress;
+    Decompress_t Decompress;
+
+    // runtime core methods
     SleepHR_t SleepHR;
     Hide_t    Hide;
     Recover_t Recover;
