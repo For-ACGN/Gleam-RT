a91bd4a0392fe54fc54b96a2d4c63da10d8ef276
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-28 13:15:31 +0800

    lib_memory.h: add mem_cmp for compare memory data with size.

--- include/lib_memory.h
+++ include/lib_memory.h
@@ -17,8 +17,14 @@ void mem_init(void* ptr, uint num);
 // mem_set is used to fill the memory with value.
 void mem_set(void* ptr, byte val, uint num);
 
+// mem_cmp is used to compare memory data with size.
+// if a = b, return 0
+// if a > b, return 1
+// if a < b, return -1
+int mem_cmp(void* a, void* b, uint size);
+
 // mem_equal is used to compare the memory is equal.
-bool mem_equal(void* dst, void* src, uint size);
+bool mem_equal(void* a, void* b, uint size);
 
 // mem_zero is used to check the memory are all zero.
 bool mem_is_zero(void* ptr, uint size);
