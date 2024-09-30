520a9eb958357ba7985c73c05991ba887e1fe5dd
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-30 13:24:58 +0800

    build.h: add macro NOT_CAMOUFLAGE that disable all modules about camouflage for debug.

--- builder/build.h
+++ builder/build.h
@@ -1,10 +1,12 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-// RELEASE_MODE: remove debug modules for generate shellcode
-// FAST_CRYPTO:  replace original algorithm to xor for test
+// RELEASE_MODE:   remove debug modules for generate shellcode
+// FAST_CRYPTO:    replace original algorithm to xor for test
+// NOT_CAMOUFLAGE: disable all modules about camouflage for debug
 
 #define RELEASE_MODE
 // #define FAST_CRYPTO
+// #define NOT_CAMOUFLAGE
 
 #endif // BUILD_H
