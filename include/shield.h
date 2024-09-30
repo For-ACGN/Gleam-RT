9bf025e435275d60037ede631b4d5d73da2a6255
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-30 12:44:55 +0800

    shield.h: replace hProcess to hTimer, remove SleepTime and Shield_Stub.

--- include/shield.h
+++ include/shield.h
@@ -8,19 +8,11 @@ typedef struct {
     uintptr BeginAddress;
     uintptr EndAddress;
     byte    CryptoKey[32];
-
-    uint32  SleepTime;
-    HANDLE  hProcess;
+    HANDLE  hTimer;
 
     WaitForSingleObject_t WaitForSingleObject;
 } Shield_Ctx;
 
 bool DefenseRT(Shield_Ctx* ctx);
-
-// reserve stub for generate random shield instructions
-#pragma warning(push)
-#pragma warning(disable: 4276)
-extern void Shield_Stub();
-#pragma warning(pop)
 
 #endif // SHIELD_H
