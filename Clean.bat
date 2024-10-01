1e694dac0f7ecb9f99aea33f860623bf1b285088
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-01 14:28:24 +0800

    add clean command about delete build output for shellcode loader.

--- Clean.bat
+++ Clean.bat
@@ -16,3 +16,8 @@ rd /S /Q "test\Debug"
 rd /S /Q "test\Release"
 rd /S /Q "test\x64"
 rd /S /Q "test\x86"
+
+rd /S /Q "tool\sc_loader\Debug"
+rd /S /Q "tool\sc_loader\Release"
+rd /S /Q "tool\sc_loader\x64"
+rd /S /Q "tool\sc_loader\x86"
