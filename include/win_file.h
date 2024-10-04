942d09fe069365059516dea9853d4cccf2d47709
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-04 17:12:30 +0800

    win_file.h: adjust module level, update comment and add Uninstall.

--- include/win_file.h
+++ include/win_file.h
@@ -4,20 +4,24 @@
 #include "c_types.h"
 #include "windows_t.h"
 #include "context.h"
+#include "errno.h"
 
-// The memory that from ReadFile will not be encrypted,
-// use it as quickly as possible, then call FreeBuf.
+// The memory buffer that from ReadFile must call Runtime_M.MemFree.
 
 typedef bool (*WFReadFileA_t)(LPSTR path, byte** buf, uint* size);
 typedef bool (*WFReadFileW_t)(LPWSTR path, byte** buf, uint* size);
 typedef bool (*WFWriteFileA_t)(LPSTR path, byte* buf, uint size);
 typedef bool (*WFWriteFileW_t)(LPWSTR path, byte* buf, uint size);
 
+typedef errno (*WFUninstall_t)();
+
 typedef struct {
     WFReadFileA_t  ReadFileA;
     WFReadFileW_t  ReadFileW;
     WFWriteFileA_t WriteFileA;
     WFWriteFileW_t WriteFileW;
+
+    WFUninstall_t Uninstall;
 } WinFile_M;
 
 WinFile_M* InitWinFile(Context* context);
