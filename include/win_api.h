ad1f0935553c28b2a3a5435d3d59cc2d49f2cc8f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-12 14:00:33 +0800

    win_api.h: remove functions and structures about PE image, improve GetModuleFileName.

--- include/win_api.h
+++ include/win_api.h
@@ -4,23 +4,6 @@
 #include "c_types.h"
 #include "windows_t.h"
 
-#define PE_FILE_HEADER_SIZE    24
-#define PE_SECTION_HEADER_SIZE 40
-
-typedef struct {
-    // optional header
-    uintptr EntryPoint;
-    uintptr ImageBase;
-    uint32  ImageSize;
-
-    // section information
-    uint32 TextVirtualSize;
-    uint32 TextVirtualAddress;
-    uint32 TextSizeOfRawData;
-    uint32 TextPointerToRawData;
-} PE_Info;
-
-uint16 GetModuleFileName(HMODULE hModule, byte* name, uint16 maxSize);
-void   ParsePEImage(byte* address, PE_Info* info);
+DWORD GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
 
 #endif // WIN_API_H
