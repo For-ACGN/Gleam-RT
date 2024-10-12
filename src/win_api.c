55b492908abb1ac56841f1e0678a228bc9dabd53
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-12 14:01:48 +0800

    win_api.c: improve GetModuleFileName about compare module name length, remove functions about PE image.

--- src/win_api.c
+++ src/win_api.c
@@ -3,7 +3,7 @@
 #include "lib_memory.h"
 #include "win_api.h"
 
-uint16 GetModuleFileName(HMODULE hModule, byte* name, uint16 maxSize)
+DWORD GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize)
 {
 #ifdef _WIN64
     uintptr peb = __readgsqword(96);
@@ -39,49 +39,12 @@ uint16 GetModuleFileName(HMODULE hModule, byte* name, uint16 maxSize)
     #elif _WIN32
         uint16 nameLen = *(uint16*)(mod + 38);
     #endif
-        if (nameLen > maxSize)
+        if (nameLen > nSize)
         {
-            nameLen = maxSize;
+            nameLen = (uint16)nSize;
         }
-        mem_copy(name, (byte*)modName, nameLen);
+        mem_copy(lpFilename, (LPWSTR)modName, nameLen);
         return nameLen;
     }
     return 0;
-}
-
-void ParsePEImage(byte* address, PE_Info* info)
-{
-    uintptr imageAddr = (uintptr)address;
-    uint32  peOffset  = *(uint32*)(imageAddr + 60);
-    // parse file header
-    uint16 numSections   = *(uint16*)(imageAddr + peOffset + 6);
-    uint16 optHeaderSize = *(uint16*)(imageAddr + peOffset + 20);
-    // parse optional header
-    uint32  entryPoint = *(uint32*)(imageAddr + peOffset + 40);
-#ifdef _WIN64
-    uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 48);
-#elif _WIN32
-    uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 52);
-#endif
-    uint32 imageSize = *(uint32*)(imageAddr + peOffset + 80);
-    // parse sections and search .text
-    uintptr section = imageAddr + PE_FILE_HEADER_SIZE + peOffset + optHeaderSize;
-    for (uint16 i = 0; i < numSections; i++)
-    {
-        // not record the original ".text" bytes
-        uint64 name = *(uint64*)section ^ 0x000000FFFFFFFFFF;
-        if (name != (0x000000747865742E ^ 0x000000FFFFFFFFFF))
-        {
-            section += PE_SECTION_HEADER_SIZE;
-            continue;
-        }
-        info->TextVirtualSize      = *(uint32*)(section + 8); 
-        info->TextVirtualAddress   = *(uint32*)(section + 12); 
-        info->TextSizeOfRawData    = *(uint32*)(section + 16); 
-        info->TextPointerToRawData = *(uint32*)(section + 20);
-        break;
-    }
-    info->EntryPoint = imageAddr + entryPoint;
-    info->ImageBase  = imageBase;
-    info->ImageSize  = imageSize;
 }
