61c8ef389544b8897dbd5cf8bcf207d084e881d8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-15 21:17:19 +0800

    build.h: add SHELLCODE_MODE for run unit tests under runtime shellcode.

--- test/build.h
+++ test/build.h
@@ -2,11 +2,17 @@
 #define BUILD_H
 
 // RELEASE_MODE:   remove debug modules for generate shellcode
+// SHELLCODE_MODE: run unit tests under runtime shellcode
 // FAST_CRYPTO:    replace original algorithm to xor for test
 // NOT_CAMOUFLAGE: disable all modules about camouflage for debug
 
 // #define RELEASE_MODE
+#define SHELLCODE_MODE
 // #define FAST_CRYPTO
 // #define NOT_CAMOUFLAGE
+
+#ifdef SHELLCODE_MODE
+  #define RELEASE_MODE
+#endif // SHELLCODE_MODE
 
 #endif // BUILD_H
