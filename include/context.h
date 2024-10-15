6aa9800ee6b6a28f0858f2189f53b098c4e89976
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-15 17:24:35 +0800

    context.h: add typedef mt_msize_t, add more fields about msize.

--- include/context.h
+++ include/context.h
@@ -13,6 +13,7 @@ typedef void* (*mt_malloc_t)(uint size);
 typedef void* (*mt_calloc_t)(uint num, uint size);
 typedef void* (*mt_realloc_t)(void* ptr, uint size);
 typedef void  (*mt_free_t)(void* ptr);
+typedef uint  (*mt_msize_t)(void* ptr);
 
 typedef struct {
     // runtime options
@@ -28,6 +29,7 @@ typedef struct {
     calloc_t  calloc;
     realloc_t realloc;
     free_t    free;
+    msize_t   msize;
 
     rt_lock_t   lock;
     rt_unlock_t unlock;
@@ -49,6 +51,7 @@ typedef struct {
     mt_calloc_t  mt_calloc;
     mt_realloc_t mt_realloc;
     mt_free_t    mt_free;
+    mt_msize_t   mt_msize;
 } Context;
 
 #endif // CONTEXT_H
