83ab349185ce7fc3a4f2af06c1519820fc3ac861
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-15 21:21:59 +0800

    runtime_test.c: update TestInitRuntime for support run tests under runtime shellcode.

--- test/runtime_test.c
+++ test/runtime_test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include "build.h"
 #include "c_types.h"
+#include "windows_t.h"
+#include "lib_memory.h"
+#include "rel_addr.h"
+#include "hash_api.h"
 #include "errno.h"
+#include "argument.h"
 #include "runtime.h"
 #include "test.h"
+
+static void* copyShellcode();
+static void* calcEpilogue();
 
 bool TestInitRuntime()
 {
@@ -12,7 +21,13 @@ bool TestInitRuntime()
         .NotAdjustProtect    = false,
         .TrackCurrentThread  = false,
     };
+#ifdef SHELLCODE_MODE
+    typedef Runtime_M* (*InitRuntime_t)(Runtime_Opts* opts);
+    InitRuntime_t initRuntime = copyShellcode();
+    runtime = initRuntime(&opts);
+#else
     runtime = InitRuntime(&opts);
+#endif // SHELLCODE_MODE
     if (runtime == NULL)
     {
         printf_s("failed to initialize runtime: 0x%X\n", GetLastErrno());
@@ -36,4 +51,30 @@ bool TestRuntime_Exit()
         return false;
     }
     return true;
+}
+
+static void* copyShellcode()
+{
+    VirtualAlloc_t VirtualAlloc = FindAPI_A("kernel32.dll", "VirtualAlloc");
+
+    uintptr begin = (uintptr)(&InitRuntime);
+    uintptr end   = (uintptr)(calcEpilogue());
+    uintptr size  = end - begin;
+    void* mem = VirtualAlloc(NULL, size, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (mem == NULL)
+    {
+        printf_s("failed to allocate memory: 0x%X\n", GetLastErrno());
+        return NULL;
+    }
+    mem_copy(mem, (void*)begin, size);
+    printf_s("shellcode: 0x%zX\n", (uintptr)mem);
+    return mem;
+}
+
+static void* calcEpilogue()
+{
+    uintptr stub = (uintptr)(GetFuncAddr(&Argument_Stub));
+    uint32  size = *(uint32*)(stub + ARG_OFFSET_ARGS_SIZE);
+    size += ARG_OFFSET_FIRST_ARG;
+    return (void*)(stub + size);
 }
