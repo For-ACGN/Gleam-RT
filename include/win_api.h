21b9dd216a56d488f15f7c2a17971318db0b58fe
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-30 14:02:01 +0800

    add HMODULE GetModuleHandle(LPWSTR lpFilename) for check module is loaded.

--- include/win_api.h
+++ include/win_api.h
@@ -4,6 +4,7 @@
 #include "c_types.h"
 #include "windows_t.h"
 
-DWORD GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
+DWORD   GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
+HMODULE GetModuleHandle(LPWSTR lpFilename);
 
 #endif // WIN_API_H
