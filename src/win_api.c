8cd346d6f2969a8b9a9feb70192598eecc81883d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-30 14:02:42 +0800

    add and implement GetModuleHandle, #include lib_string.h.

--- src/win_api.c
+++ src/win_api.c
@@ -1,6 +1,7 @@
 #include "c_types.h"
 #include "windows_t.h"
 #include "lib_memory.h"
+#include "lib_string.h"
 #include "win_api.h"
 
 DWORD GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize)
@@ -47,4 +48,40 @@ DWORD GetModuleFileName(HMODULE hModule, LPWSTR lpFilename, DWORD nSize)
         return nameLen;
     }
     return 0;
+}
+
+HMODULE GetModuleHandle(LPWSTR lpFilename)
+{
+#ifdef _WIN64
+    uintptr peb = __readgsqword(96);
+    uintptr ldr = *(uintptr*)(peb + 24);
+    uintptr mod = *(uintptr*)(ldr + 32);
+#elif _WIN32
+    uintptr peb = __readfsdword(48);
+    uintptr ldr = *(uintptr*)(peb + 12);
+    uintptr mod = *(uintptr*)(ldr + 20);
+#endif
+    for (;; mod = *(uintptr*)(mod))
+    {
+    #ifdef _WIN64
+        uintptr modName = *(uintptr*)(mod + 80);
+    #elif _WIN32
+        uintptr modName = *(uintptr*)(mod + 40);
+    #endif
+        if (modName == 0x00)
+        {
+            break;
+        }
+        if (strcmp_w((LPWSTR)modName, lpFilename) != 0)
+        {
+            continue;
+        }
+    #ifdef _WIN64
+        uintptr modBase = *(uintptr*)(mod + 32);
+    #elif _WIN32
+        uintptr modBase = *(uintptr*)(mod + 16);
+    #endif
+        return (HMODULE)modBase;
+    }
+    return NULL;
 }
