3530dfb2c4d907fbf44655fed6a82adc4dfbb068
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-03 13:35:05 +0800

    msvcrt_t.h: add __cdecl before functions for fix bug about memory tracker.

--- include/msvcrt_t.h
+++ include/msvcrt_t.h
@@ -3,22 +3,22 @@
 
 #include "c_types.h"
 
-typedef void* (*msvcrt_malloc_t)
+typedef void* (__cdecl *msvcrt_malloc_t)
 (
 	uint size
 );
 
-typedef void* (*msvcrt_calloc_t)
+typedef void* (__cdecl *msvcrt_calloc_t)
 (
 	uint num, uint size
 );
 
-typedef void* (*msvcrt_realloc_t)
+typedef void* (__cdecl *msvcrt_realloc_t)
 (
 	void* ptr, uint size
 );
 
-typedef void (*msvcrt_free_t)
+typedef void (__cdecl *msvcrt_free_t)
 (
 	void* ptr
 );
