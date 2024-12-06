3bc00078f66cbe7f991f0ec2ed0db2f107760b19
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-06 19:56:31 +0800

    memory_test.c: improve test structure, add TestMemory_Msvcrt.

--- test/src/memory_test.c
+++ test/src/memory_test.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include "c_types.h"
+#include "msvcrt_t.h"
 #include "errno.h"
 #include "runtime.h"
 #include "test.h"
 
+static bool TestMemory_Virtual();
+static bool TestMemory_Heap();
+static bool TestMemory_Msvcrt();
+static bool TestMemory_Ucrtbase();
+
 bool TestRuntime_Memory()
+{
+    test_t tests[] = {
+        { TestMemory_Virtual  },
+        { TestMemory_Heap     },
+        { TestMemory_Msvcrt   },
+        { TestMemory_Ucrtbase },
+    };
+    for (int i = 0; i < arrlen(tests); i++)
+    {
+        printf_s("--------------------------------\n");
+        if (!tests[i]())
+        {
+            return false;
+        }
+        printf_s("--------------------------------\n\n");
+    }
+    return true;
+}
+
+bool TestMemory_Virtual()
 {
     uint64* test1 = (uint64*)runtime->Memory.Alloc(sizeof(uint64));
     if (test1 == NULL)
@@ -79,5 +105,45 @@ bool TestRuntime_Memory()
     runtime->Memory.Free(test2);
     runtime->Memory.Free(test4);
     runtime->Memory.Free(test5);
+    return true;
+}
+
+static bool TestMemory_Heap()
+{
+    return true;
+}
+
+static bool TestMemory_Msvcrt()
+{
+    HMODULE hModule = runtime->Library.LoadA("msvcrt.dll");
+
+    msvcrt_malloc_t  malloc  = runtime->Library.GetProc(hModule, "malloc");
+    msvcrt_calloc_t  calloc  = runtime->Library.GetProc(hModule, "calloc");
+    msvcrt_realloc_t realloc = runtime->Library.GetProc(hModule, "realloc");
+    msvcrt_free_t    free    = runtime->Library.GetProc(hModule, "free");
+
+    uint* test1 = malloc(8);
+    uint* test2 = calloc(4, 8);
+    uint* test3 = realloc(test1, 27);
+    
+    *test2 = 0x5678;
+    *test3 = 0x1212;
+
+    runtime->Core.Sleep(10);
+    free(test2);
+    runtime->Core.Sleep(10);
+    free(test3);
+    runtime->Core.Sleep(10);
+
+    // not free
+    test1 = malloc(8);
+    *test1 = 0x1234;
+    runtime->Core.Sleep(10);
+
+    return runtime->Library.Free(hModule);
+}
+
+static bool TestMemory_Ucrtbase()
+{
     return true;
 }
