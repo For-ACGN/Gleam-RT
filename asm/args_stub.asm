abc9e134bb5ec50663bf3a20e61d3370e9a5d8db
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-06 19:33:06 +0800

    args_stub.asm: replace old stub to the new that use new algorithm.

--- asm/args_stub.asm
+++ asm/args_stub.asm
@@ -16,41 +16,25 @@ ELSE
   Argument_Stub proc
 ENDIF
 
-  ; 32 bytes decrypt key
-  db 068h, 005h, 015h, 066h
-  db 0C7h, 099h, 007h, 00Bh
-  db 0BDh, 0D0h, 0F1h, 0E1h
-  db 0ADh, 07Dh, 08Bh, 0F8h
-  db 061h, 07Bh, 07Bh, 0BAh
-  db 03Ch, 05Eh, 04Eh, 035h
-  db 018h, 0AEh, 0BAh, 094h
-  db 080h, 005h, 057h, 0B3h
-
-  ; record the number of the arguments
-  db 003h, 000h, 000h, 000h
-
-  ; record the total argument data size
-  db 01Ch, 000h, 000h, 000h
-
-  ; 4 bytes checksum for check header
-  db 054h, 064h, 09Bh, 081h
-
-  ; record the size of the argument-1
-  db 093h, 001h, 015h, 066h
-  ; argument-1 data
-  db 0BFh, 0B7h, 065h, 02Dh
-
-  ; record the size of the argument-2
-  db 0A3h, 0DCh, 0F1h, 0E1h
-  ; argument-2 data
-  db 0CCh, 07Dh, 08Bh, 0F8h
-  db 062h, 07Bh, 07Bh, 0BAh
-  db 03Dh, 05Eh, 04Eh, 056h
-
-  ; record the size of the argument-3
-  db 018h, 0AEh, 0BAh, 094h
-  ; argument-3 data(empty)
-
+  db 0E7h, 09Fh, 03Eh, 006h    ; 32 bytes decrypt key
+  db 0F6h, 0A5h, 08Bh, 06Eh
+  db 020h, 022h, 0F4h, 03Eh
+  db 0E9h, 0B5h, 00Bh, 08Ah
+  db 0E6h, 0ABh, 016h, 035h
+  db 010h, 0BBh, 038h, 0A6h
+  db 0CDh, 0A8h, 0B6h, 0C1h
+  db 0CDh, 00Ah, 0B0h, 015h
+  db 003h, 000h, 000h, 000h    ; record the number of the arguments
+  db 01Ch, 000h, 000h, 000h    ; record the total argument data size
+  db 073h, 0A7h, 036h, 0A4h    ; 4 bytes checksum for check header
+  db 061h, 00Bh, 00Bh, 003h    ; record the size of the argument-1
+  db 03Dh, 0FFh, 0F9h, 013h    ; argument-1 data
+  db 035h, 040h, 029h, 00Bh    ; record the size of the argument-2
+  db 016h, 029h, 047h, 04Ah    ; argument-2 data
+  db 08Fh, 0B4h, 053h, 001h
+  db 07Fh, 039h, 0F1h, 024h
+  db 0FAh, 009h, 017h, 00Eh    ; record the size of the argument-3
+                               ; argument-3 data (empty)
 IFDEF _WIN32
   _Argument_Stub@0 endp
 ELSE
